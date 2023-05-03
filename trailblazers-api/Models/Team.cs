e0b210441d625ed9669ed9c06881a1d56833b2b6
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-03 23:21:07 +0800

    Add Team model

---
+++ trailblazers-api/Models/Team.cs
@@ -0,0 +1,13 @@
+﻿namespace trailblazers_api.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BuildOneId { get; set; }
+        public int BuildTwoId { get; set; }
+        public int BuildThreeId { get; set; }
+        public int BuildFourId { get; set; }
+
+    }
+}
