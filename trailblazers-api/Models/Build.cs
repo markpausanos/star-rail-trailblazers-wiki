1bcf287ef3a2407192b7a556a9f06e20eded0fc4
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-03 23:21:01 +0800

    Add Build model

---
+++ trailblazers-api/Models/Build.cs
@@ -0,0 +1,11 @@
+﻿namespace trailblazers_api.Models
+{
+    public class Build
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int RelicOneId { get; set; }
+        public int RelicTwoId { get; set; }
+        public int OrnamentId { get; set; }
+    }
+}
