c783375716414cac33555745bb7ff0308c0793ff
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-03 23:15:36 +0800

    Add Ornament model

---
+++ trailblazers-api/Models/Ornament.cs
@@ -0,0 +1,11 @@
+﻿namespace trailblazers_api.Models
+{
+    public class Ornament
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public string? BonusEffectDescription { get; set; }
+    }
+}
