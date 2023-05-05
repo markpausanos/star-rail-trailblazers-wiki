66a1f8c738dcf079768116899c3368a4f494d47c
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-06 00:17:36 +0800

    Add Lightcone repository

---
+++ trailblazers-api/trailblazers-api/Repositories/Lightcones/ILightconeRepository.cs
@@ -0,0 +1,49 @@
+﻿using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Lightcones
+{
+    public interface ILightconeRepository
+    {
+        /// <summary>
+        /// Create a new Lightcone in the Database.
+        /// </summary>
+        /// <param name="lightcone">New Lightcone to be created.</param>
+        /// <returns>A int Data type which is the Id of the newly created Lightcone</returns>
+        Task<int> CreateLightcone(Lightcone lightcone);
+        /// <summary>
+        /// Gets all Lightcone in the databse.
+        /// </summary>
+        /// <returns><IEnumerable<Lightcone>></returns>
+        Task<IEnumerable<Lightcone>> GetAllLightcones();
+        /// <summary>
+        /// Gets Lightcone in the database by the Id.
+        /// </summary>
+        /// <param name="id">Id of the Lightcone to get in the database.</param>
+        /// <returns>A nullable Lightcone</returns>
+        Task<Lightcone?> GetLightconeById(int id);
+        /// <summary>
+        /// Gets a Lightcone in the databse by Name.
+        /// </summary>
+        /// <param name="name">Name of the Lightcone to get.</param>
+        /// <returns>A nullable Lightcone</returns>
+        Task<Lightcone?> GetLightconeByName(string name);
+        /// <summary>
+        /// Updates a Lightcone in the database.
+        /// </summary>
+        /// <param name="lightcone">Updated Lightcone</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> UpdateLightcone(Lightcone lightcone);
+        /// <summary>
+        /// Deletes a Lightcone in the database.
+        /// </summary>
+        /// <param name="id">Id of the Lightcone to be Deleted.</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> DeleteLightcone(int id);
+    }
+}
---
+++ trailblazers-api/trailblazers-api/Repositories/Lightcones/LightconeRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using trailblazers_api.Context;
+using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Lightcones
+{
+    public class LightconeRepository : ILightconeRepository
+    {
+        private readonly DapperContext _context;
+
+        public LightconeRepository(DapperContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CreateLightcone(Lightcone lightcone)
+        {
+            var sql = "INSERT INTO Lightcones (Name, Description, Image) VALUES (@Name, @Description, @Image); " +
+                      "SELECT SCOPE_IDENTITY();";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteScalarAsync<int>(sql, new { lightcone.Name, lightcone.Description, lightcone.Image });
+            }
+        }
+        public async Task<IEnumerable<Lightcone>> GetAllLightcones()
+        {
+            var sql = "SELECT * FROM Lightcones;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QueryAsync<Lightcone>(sql);
+            }
+        }
+        public async Task<Lightcone?> GetLightconeById(int id)
+        {
+            var sql = "SELECT * FROM Lightcones WHERE Id = @Id;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<Lightcone>(sql, new { id });
+            }
+        }
+        public async Task<Lightcone?> GetLightconeByName(string name)
+        {
+            var sql = "SELECT * FROM Lightcones WHERE Name = @Name;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<Lightcone>(sql, new { name });
+            }
+        }
+        public async Task<bool> UpdateLightcone(Lightcone lightcone)
+        {
+            var sql = "UPDATE Lightcones SET Description = @Description WHERE Id = @Id;";
+
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteAsync(sql, new { lightcone.Description, lightcone.Id }) > 0;
+            }
+        }
+        public Task<bool> DeleteLightcone(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
