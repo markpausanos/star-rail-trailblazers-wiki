f1ea8f05ebc8658a28740ad8d4984704a314cc0b
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-06 00:29:31 +0800

    Add User repository

---
+++ trailblazers-api/trailblazers-api/Repositories/Users/IUserRepository.cs
@@ -0,0 +1,49 @@
+﻿using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Users
+{
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Create a new User in the Database.
+        /// </summary>
+        /// <param name="user">New User to be created.</param>
+        /// <returns>A int Data type which is the Id of the newly created User</returns>
+        Task<int> CreateUser(User user);
+        /// <summary>
+        /// Gets all User in the databse.
+        /// </summary>
+        /// <returns><IEnumerable<User>></returns>
+        Task<IEnumerable<User>> GetAllUsers();
+        /// <summary>
+        /// Gets User in the database by the Id.
+        /// </summary>
+        /// <param name="id">Id of the User to get in the database.</param>
+        /// <returns>A nullable User</returns>
+        Task<User?> GetUserById(int id);
+        /// <summary>
+        /// Gets a User in the databse by Name.
+        /// </summary>
+        /// <param name="name">Name of the User to get.</param>
+        /// <returns>A nullable User</returns>
+        Task<User?> GetUserByName(string name);
+        /// <summary>
+        /// Updates a User in the database.
+        /// </summary>
+        /// <param name="user">Updated User</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> UpdateUser(User user);
+        /// <summary>
+        /// Deletes a User in the database.
+        /// </summary>
+        /// <param name="id">Id of the User to be Deleted.</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> DeleteUser(int id);
+    }
+}
---
+++ trailblazers-api/trailblazers-api/Repositories/Users/UserRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using trailblazers_api.Context;
+using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Users
+{
+    public class UserRepository
+    {
+        private readonly DapperContext _context;
+
+        public UserRepository(DapperContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CreateUser(User user)
+        {
+            var sql = "INSERT INTO Users (Name, Description, Image) VALUES (@Name, @Description, @Image); " +
+                      "SELECT SCOPE_IDENTITY();";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteScalarAsync<int>(sql, new { user.Name, user.Description, user.Image });
+            }
+        }
+        public async Task<IEnumerable<User>> GetAllUsers()
+        {
+            var sql = "SELECT * FROM Users;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QueryAsync<User>(sql);
+            }
+        }
+        public async Task<User?> GetUserById(int id)
+        {
+            var sql = "SELECT * FROM Users WHERE Id = @Id;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<User>(sql, new { id });
+            }
+        }
+        public async Task<User?> GetUserByName(string name)
+        {
+            var sql = "SELECT * FROM Users WHERE Name = @Name;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<User>(sql, new { name });
+            }
+        }
+        public async Task<bool> UpdateUser(User user)
+        {
+            var sql = "UPDATE Users SET Description = @Description WHERE Id = @Id;";
+
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteAsync(sql, new { user.Description, user.Id }) > 0;
+            }
+        }
+        public Task<bool> DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
