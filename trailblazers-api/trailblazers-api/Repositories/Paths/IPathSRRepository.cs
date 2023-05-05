95b193c5692c16c57433cfe6e79d8165493917ae
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-05 20:55:17 +0800

    Add Path repository

---
+++ trailblazers-api/trailblazers-api/Repositories/Paths/IPathSRRepository.cs
@@ -0,0 +1,49 @@
+﻿using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Paths
+{
+    public interface IPathSRRepository
+    {
+        /// <summary>
+        /// Create a new PathSR in the Database.
+        /// </summary>
+        /// <param name="path">New PathSR to be created.</param>
+        /// <returns>A int Data type which is the Id of the newly created PathSR</returns>
+        Task<int> CreatePathSR(PathSR path);
+        /// <summary>
+        /// Gets all PathSR in the databse.
+        /// </summary>
+        /// <returns><IEnumerable<PathSR>></returns>
+        Task<IEnumerable<PathSR>> GetAllPathSRs();
+        /// <summary>
+        /// Gets PathSR in the database by the Id.
+        /// </summary>
+        /// <param name="id">Id of the PathSR to get in the database.</param>
+        /// <returns>A nullable PathSR</returns>
+        Task<PathSR?> GetPathSRById(int id);
+        /// <summary>
+        /// Gets a PathSR in the databse by Name.
+        /// </summary>
+        /// <param name="name">Name of the PathSR to get.</param>
+        /// <returns>A nullable PathSR</returns>
+        Task<PathSR?> GetPathSRByName(string name);
+        /// <summary>
+        /// Updates a PathSR in the database.
+        /// </summary>
+        /// <param name="path">Updated PathSR</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> UpdatePathSR(PathSR path);
+        /// <summary>
+        /// Deletes a PathSR in the database.
+        /// </summary>
+        /// <param name="id">Id of the PathSR to be Deleted.</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> DeletePathSR(int id);
+    }
+}
---
+++ trailblazers-api/trailblazers-api/Repositories/Paths/PathSRRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using trailblazers_api.Context;
+using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Paths
+{
+    public class PathSRRepository : IPathSRRepository
+    {
+        private readonly DapperContext _context;
+
+        public PathSRRepository(DapperContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CreatePathSR(PathSR path)
+        {
+            var sql = "INSERT INTO Paths (Name, Description, Image) VALUES (@Name, @Description, @Image); " +
+                      "SELECT SCOPE_IDENTITY();";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteScalarAsync<int>(sql, new { path.Name, path.Description, path.Image });
+            }
+        }
+        public async Task<IEnumerable<PathSR>> GetAllPaths()
+        {
+            var sql = "SELECT * FROM Paths;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QueryAsync<PathSR>(sql);
+            }
+        }
+        public async Task<PathSR?> GetPathSRById(int id)
+        {
+            var sql = "SELECT * FROM Paths WHERE Id = @Id;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<PathSR>(sql, new { id });
+            }
+        }
+        public async Task<PathSR?> GetPathSRByName(string name)
+        {
+            var sql = "SELECT * FROM Paths WHERE Name = @Name;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<PathSR>(sql, new { name });
+            }
+        }
+        public async Task<bool> UpdatePathSR(PathSR path)
+        {
+            var sql = "UPDATE Paths SET Description = @Description WHERE Id = @Id;";
+
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteAsync(sql, new { path.Description, path.Id }) > 0;
+            }
+        }
+        public Task<bool> DeletePathSR(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
