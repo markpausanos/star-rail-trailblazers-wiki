99b10c036d255dadbbf730d4c03cb386abe433ec
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-05 19:26:19 +0800

    Add Like model

---
+++ trailblazers-api/trailblazers-api/Models/Like.cs
@@ -0,0 +1,10 @@
+﻿namespace trailblazers_api.Models
+{
+    public class Like
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int TeamId { get; set; }
+        public bool IsLike { get; set; }
+    }
+}
