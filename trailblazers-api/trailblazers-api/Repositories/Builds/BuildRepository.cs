85818587d164f7f50135151d124c3fcd91097114
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-06 00:21:44 +0800

    Add Build repository

---
+++ trailblazers-api/trailblazers-api/Repositories/Builds/BuildRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using trailblazers_api.Context;
+using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Builds
+{
+    public class BuildRepository : IBuildRepository
+    {
+        private readonly DapperContext _context;
+
+        public BuildRepository(DapperContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CreateBuild(Build build)
+        {
+            var sql = "INSERT INTO Builds (Name, Description, Image) VALUES (@Name, @Description, @Image); " +
+                      "SELECT SCOPE_IDENTITY();";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteScalarAsync<int>(sql, new { build.Name, build.Description, build.Image });
+            }
+        }
+        public async Task<IEnumerable<Build>> GetAllBuilds()
+        {
+            var sql = "SELECT * FROM Builds;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QueryAsync<Build>(sql);
+            }
+        }
+        public async Task<Build?> GetBuildById(int id)
+        {
+            var sql = "SELECT * FROM Builds WHERE Id = @Id;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<Build>(sql, new { id });
+            }
+        }
+        public async Task<Build?> GetBuildByName(string name)
+        {
+            var sql = "SELECT * FROM Builds WHERE Name = @Name;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<Build>(sql, new { name });
+            }
+        }
+        public async Task<bool> UpdateBuild(Build build)
+        {
+            var sql = "UPDATE Builds SET Description = @Description WHERE Id = @Id;";
+
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteAsync(sql, new { build.Description, build.Id }) > 0;
+            }
+        }
+        public Task<bool> DeleteBuild(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trailblazers-api/trailblazers-api/Repositories/Builds/IBuildRepository.cs
@@ -0,0 +1,49 @@
+﻿using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Builds
+{
+    public interface IBuildRepository
+    {
+        /// <summary>
+        /// Create a new Build in the Database.
+        /// </summary>
+        /// <param name="build">New Build to be created.</param>
+        /// <returns>A int Data type which is the Id of the newly created Build</returns>
+        Task<int> CreateBuild(Build build);
+        /// <summary>
+        /// Gets all Build in the databse.
+        /// </summary>
+        /// <returns><IEnumerable<Build>></returns>
+        Task<IEnumerable<Build>> GetAllBuilds();
+        /// <summary>
+        /// Gets Build in the database by the Id.
+        /// </summary>
+        /// <param name="id">Id of the Build to get in the database.</param>
+        /// <returns>A nullable Build</returns>
+        Task<Build?> GetBuildById(int id);
+        /// <summary>
+        /// Gets a Build in the databse by Name.
+        /// </summary>
+        /// <param name="name">Name of the Build to get.</param>
+        /// <returns>A nullable Build</returns>
+        Task<Build?> GetBuildByName(string name);
+        /// <summary>
+        /// Updates a Build in the database.
+        /// </summary>
+        /// <param name="build">Updated Build</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> UpdateBuild(Build build);
+        /// <summary>
+        /// Deletes a Build in the database.
+        /// </summary>
+        /// <param name="id">Id of the Build to be Deleted.</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> DeleteBuild(int id);
+    }
+}
