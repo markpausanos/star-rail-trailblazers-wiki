339108a2f83fb0a11a4c89bbd0505b167d6728ff
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-05 22:13:23 +0800

    Update Trailblazer model

--- trailblazers-api/trailblazers-api/Models/Trailblazer.cs
+++ trailblazers-api/trailblazers-api/Models/Trailblazer.cs
@@ -6,12 +6,12 @@
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string? Image { get; set; }
-        public Element? Element { get; set; }
-        public PathSR? Path{ get; set; }
         public int Rarity { get; set; }
         public int BaseHp { get; set; }
         public int BaseAtk { get; set; }
         public int BaseDef { get; set; }
         public int BaseSpeed { get; set; }
+        public Element? Element { get; set; }
+        public PathSR? Path{ get; set; }
     }
 }
