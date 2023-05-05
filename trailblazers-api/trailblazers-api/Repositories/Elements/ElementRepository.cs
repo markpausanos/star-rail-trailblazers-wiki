f1e284457be258f446f5f59731fda76931fd3c44
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-05 19:40:29 +0800

    Add Element repository

---
+++ trailblazers-api/trailblazers-api/Repositories/Elements/ElementRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using trailblazers_api.Context;
+using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Elements
+{
+    public class ElementRepository : IElementRepository
+    {
+        private readonly DapperContext _context;
+
+        public ElementRepository(DapperContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CreateElement(Element element)
+        {
+            var sql = "INSERT INTO Elements (Name, Description, Image) VALUES (@Name, @Description, @Image); " +
+                      "SELECT SCOPE_IDENTITY();";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteScalarAsync<int>(sql, new { element.Name, element.Description, element.Image });
+            }
+        }
+        public async Task<IEnumerable<Element>> GetAllElements()
+        {
+            var sql = "SELECT * FROM Elements;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QueryAsync<Element>(sql);
+            }
+        }
+        public async Task<Element?> GetElementById(int id)
+        {
+            var sql = "SELECT * FROM Elements WHERE Id = @Id;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<Element>(sql, new { id });
+            }
+        }
+        public async Task<Element?> GetElementByName(string name)
+        {
+            var sql = "SELECT * FROM Elements WHERE Name = @Name;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<Element>(sql, new { name });
+            }
+        }
+        public async Task<bool> UpdateElement(Element element)
+        {
+            var sql = "UPDATE Roles SET Description = @Description WHERE Id = @Id;";
+
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteAsync(sql, new { element.Description, element.Id }) > 0;
+            }
+        }
+        public Task<bool> DeleteElement(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trailblazers-api/trailblazers-api/Repositories/Elements/IElementRepository.cs
@@ -0,0 +1,49 @@
+﻿using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Elements
+{
+    public interface IElementRepository
+    {
+        /// <summary>
+        /// Create a new Element in the Database.
+        /// </summary>
+        /// <param name="element">New Element to be created.</param>
+        /// <returns>A int Data type which is the Id of the newly created Element</returns>
+        Task<int> CreateElement(Element element);
+        /// <summary>
+        /// Gets all Element in the databse.
+        /// </summary>
+        /// <returns><IEnumerable<Element>></returns>
+        Task<IEnumerable<Element>> GetAllElements();
+        /// <summary>
+        /// Gets Element in the database by the Id.
+        /// </summary>
+        /// <param name="id">Id of the Element to get in the database.</param>
+        /// <returns>A nullable Element</returns>
+        Task<Element?> GetElementById(int id);
+        /// <summary>
+        /// Gets a Role in the databse by Name.
+        /// </summary>
+        /// <param name="name">Name of the Role to get.</param>
+        /// <returns>A nullable Role</returns>
+        Task<Element?> GetElementByName(string name);
+        /// <summary>
+        /// Updates a Element in the database.
+        /// </summary>
+        /// <param name="element">Updated Element</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> UpdateElement(Element element);
+        /// <summary>
+        /// Deletes a Element in the database.
+        /// </summary>
+        /// <param name="id">Id of the Element to be Deleted.</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> DeleteElement(int id);
+    }
+}
