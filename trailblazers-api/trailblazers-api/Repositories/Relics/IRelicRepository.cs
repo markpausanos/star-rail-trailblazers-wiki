98f9582e8da8cab8f62c0591835355d553c41f78
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-06 00:10:12 +0800

    Add Relic repository

---
+++ trailblazers-api/trailblazers-api/Repositories/Relics/IRelicRepository.cs
@@ -0,0 +1,49 @@
+﻿using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Relics
+{
+    public interface IRelicRepository
+    {
+        /// <summary>
+        /// Create a new Relic in the Database.
+        /// </summary>
+        /// <param name="relic">New Relic to be created.</param>
+        /// <returns>A int Data type which is the Id of the newly created Relic</returns>
+        Task<int> CreateRelic(Relic relic);
+        /// <summary>
+        /// Gets all Relic in the databse.
+        /// </summary>
+        /// <returns><IEnumerable<Relic>></returns>
+        Task<IEnumerable<Relic>> GetAllRelics();
+        /// <summary>
+        /// Gets Relic in the database by the Id.
+        /// </summary>
+        /// <param name="id">Id of the Relic to get in the database.</param>
+        /// <returns>A nullable Relic</returns>
+        Task<Relic?> GetRelicById(int id);
+        /// <summary>
+        /// Gets a Relic in the databse by Name.
+        /// </summary>
+        /// <param name="name">Name of the Relic to get.</param>
+        /// <returns>A nullable Relic</returns>
+        Task<Relic?> GetRelicByName(string name);
+        /// <summary>
+        /// Updates a Relic in the database.
+        /// </summary>
+        /// <param name="relic">Updated Relic</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> UpdateRelic(Relic relic);
+        /// <summary>
+        /// Deletes a Relic in the database.
+        /// </summary>
+        /// <param name="id">Id of the Relic to be Deleted.</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> DeleteRelic(int id);
+    }
+}
---
+++ trailblazers-api/trailblazers-api/Repositories/Relics/RelicRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using trailblazers_api.Context;
+using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Relics
+{
+    public class RelicRepository : IRelicRepository
+    {
+        private readonly DapperContext _context;
+
+        public RelicRepository(DapperContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CreateRelic(Relic relic)
+        {
+            var sql = "INSERT INTO Relics (Name, Description, Image) VALUES (@Name, @Description, @Image); " +
+                      "SELECT SCOPE_IDENTITY();";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteScalarAsync<int>(sql, new { relic.Name, relic.Description, relic.Image });
+            }
+        }
+        public async Task<IEnumerable<Relic>> GetAllRelics()
+        {
+            var sql = "SELECT * FROM Relics;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QueryAsync<Relic>(sql);
+            }
+        }
+        public async Task<Relic?> GetRelicById(int id)
+        {
+            var sql = "SELECT * FROM Relics WHERE Id = @Id;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<Relic>(sql, new { id });
+            }
+        }
+        public async Task<Relic?> GetRelicByName(string name)
+        {
+            var sql = "SELECT * FROM Relics WHERE Name = @Name;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<Relic>(sql, new { name });
+            }
+        }
+        public async Task<bool> UpdateRelic(Relic relic)
+        {
+            var sql = "UPDATE Relics SET Description = @Description WHERE Id = @Id;";
+
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteAsync(sql, new { relic.Description, relic.Id }) > 0;
+            }
+        }
+        public Task<bool> DeleteRelic(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
