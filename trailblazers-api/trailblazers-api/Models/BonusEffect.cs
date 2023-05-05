adfc3e43931014fc027fcbb184562a95352f9a06
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-05 19:25:33 +0800

    Add BonusEffect model

---
+++ trailblazers-api/trailblazers-api/Models/BonusEffect.cs
@@ -0,0 +1,9 @@
+﻿namespace trailblazers_api.Models
+{
+    public class BonusEffect
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
