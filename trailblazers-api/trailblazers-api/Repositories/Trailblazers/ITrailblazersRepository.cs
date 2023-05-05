e00c4700853d0ba7c33ea1f3769a32cc076b5118
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-06 00:29:24 +0800

    Add Trailblazer repository

---
+++ trailblazers-api/trailblazers-api/Repositories/Trailblazers/ITrailblazersRepository.cs
@@ -0,0 +1,49 @@
+﻿using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Trailblazers
+{
+    public interface ITrailblazersRepository
+    {
+        /// <summary>
+        /// Create a new Trailblazer in the Database.
+        /// </summary>
+        /// <param name="trailblazer">New Trailblazer to be created.</param>
+        /// <returns>A int Data type which is the Id of the newly created Trailblazer</returns>
+        Task<int> CreateTrailblazer(Trailblazer trailblazer);
+        /// <summary>
+        /// Gets all Trailblazer in the databse.
+        /// </summary>
+        /// <returns><IEnumerable<Trailblazer>></returns>
+        Task<IEnumerable<Trailblazer>> GetAllTrailblazers();
+        /// <summary>
+        /// Gets Trailblazer in the database by the Id.
+        /// </summary>
+        /// <param name="id">Id of the Trailblazer to get in the database.</param>
+        /// <returns>A nullable Trailblazer</returns>
+        Task<Trailblazer?> GetTrailblazerById(int id);
+        /// <summary>
+        /// Gets a Trailblazer in the databse by Name.
+        /// </summary>
+        /// <param name="name">Name of the Trailblazer to get.</param>
+        /// <returns>A nullable Trailblazer</returns>
+        Task<Trailblazer?> GetTrailblazerByName(string name);
+        /// <summary>
+        /// Updates a Trailblazer in the database.
+        /// </summary>
+        /// <param name="trailblazer">Updated Trailblazer</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> UpdateTrailblazer(Trailblazer trailblazer);
+        /// <summary>
+        /// Deletes a Trailblazer in the database.
+        /// </summary>
+        /// <param name="id">Id of the Trailblazer to be Deleted.</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> DeleteTrailblazer(int id);
+    }
+}
---
+++ trailblazers-api/trailblazers-api/Repositories/Trailblazers/TrailblazerRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using trailblazers_api.Context;
+using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Trailblazers
+{
+    public class TrailblazerRepository
+    {
+        private readonly DapperContext _context;
+
+        public TrailblazerRepository(DapperContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CreateTrailblazer(Trailblazer trailblazer)
+        {
+            var sql = "INSERT INTO Trailblazers (Name, Description, Image) VALUES (@Name, @Description, @Image); " +
+                      "SELECT SCOPE_IDENTITY();";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteScalarAsync<int>(sql, new { trailblazer.Name, trailblazer.Description, trailblazer.Image });
+            }
+        }
+        public async Task<IEnumerable<Trailblazer>> GetAllTrailblazers()
+        {
+            var sql = "SELECT * FROM Trailblazers;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QueryAsync<Trailblazer>(sql);
+            }
+        }
+        public async Task<Trailblazer?> GetTrailblazerById(int id)
+        {
+            var sql = "SELECT * FROM Trailblazers WHERE Id = @Id;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<Trailblazer>(sql, new { id });
+            }
+        }
+        public async Task<Trailblazer?> GetTrailblazerByName(string name)
+        {
+            var sql = "SELECT * FROM Trailblazers WHERE Name = @Name;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<Trailblazer>(sql, new { name });
+            }
+        }
+        public async Task<bool> UpdateTrailblazer(Trailblazer trailblazer)
+        {
+            var sql = "UPDATE Trailblazers SET Description = @Description WHERE Id = @Id;";
+
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteAsync(sql, new { trailblazer.Description, trailblazer.Id }) > 0;
+            }
+        }
+        public Task<bool> DeleteTrailblazer(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
