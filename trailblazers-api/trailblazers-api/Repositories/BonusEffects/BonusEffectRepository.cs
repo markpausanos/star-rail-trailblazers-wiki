12df4e1964fe839e5e1e6108d0cf2329774edf7c
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-05 22:52:46 +0800

    Add BonusEffect repository

---
+++ trailblazers-api/trailblazers-api/Repositories/BonusEffects/BonusEffectRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using trailblazers_api.Context;
+using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.BonusEffects
+{
+    public class BonusEffectRepository : IBonusEffectRepository
+    {
+        private readonly DapperContext _context;
+
+        public BonusEffectRepository(DapperContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CreateBonusEffect(BonusEffect element)
+        {
+            var sql = "INSERT INTO BonusEffects (Name, Description, Image) VALUES (@Name, @Description, @Image); " +
+                      "SELECT SCOPE_IDENTITY();";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteScalarAsync<int>(sql, new { element.Name, element.Description, element.Image });
+            }
+        }
+        public async Task<IEnumerable<BonusEffect>> GetAllBonusEffects()
+        {
+            var sql = "SELECT * FROM BonusEffects;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QueryAsync<BonusEffect>(sql);
+            }
+        }
+        public async Task<BonusEffect?> GetBonusEffectById(int id)
+        {
+            var sql = "SELECT * FROM BonusEffects WHERE Id = @Id;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<BonusEffect>(sql, new { id });
+            }
+        }
+        public async Task<BonusEffect?> GetBonusEffectByName(string name)
+        {
+            var sql = "SELECT * FROM BonusEffects WHERE Name = @Name;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<BonusEffect>(sql, new { name });
+            }
+        }
+        public async Task<bool> UpdateBonusEffect(BonusEffect element)
+        {
+            var sql = "UPDATE BonusEffects SET Description = @Description WHERE Id = @Id;";
+
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteAsync(sql, new { element.Description, element.Id }) > 0;
+            }
+        }
+        public Task<bool> DeleteBonusEffect(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trailblazers-api/trailblazers-api/Repositories/BonusEffects/IBonusEffectRepository.cs
@@ -0,0 +1,49 @@
+﻿using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.BonusEffects
+{
+    public interface IBonusEffectRepository
+    {
+        /// <summary>
+        /// Create a new BonusEffect in the Database.
+        /// </summary>
+        /// <param name="bonusEffect">New BonusEffect to be created.</param>
+        /// <returns>A int Data type which is the Id of the newly created BonusEffect</returns>
+        Task<int> CreateBonusEffect(BonusEffect bonusEffect);
+        /// <summary>
+        /// Gets all BonusEffect in the databse.
+        /// </summary>
+        /// <returns><IEnumerable<BonusEffect>></returns>
+        Task<IEnumerable<BonusEffect>> GetAllBonusEffects();
+        /// <summary>
+        /// Gets BonusEffect in the database by the Id.
+        /// </summary>
+        /// <param name="id">Id of the BonusEffect to get in the database.</param>
+        /// <returns>A nullable BonusEffect</returns>
+        Task<BonusEffect?> GetBonusEffectById(int id);
+        /// <summary>
+        /// Gets a BonusEffect in the databse by Name.
+        /// </summary>
+        /// <param name="name">Name of the BonusEffect to get.</param>
+        /// <returns>A nullable BonusEffect</returns>
+        Task<BonusEffect?> GetBonusEffectByName(string name);
+        /// <summary>
+        /// Updates a BonusEffect in the database.
+        /// </summary>
+        /// <param name="bonusEffect">Updated BonusEffect</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> UpdateBonusEffect(BonusEffect bonusEffect);
+        /// <summary>
+        /// Deletes a BonusEffect in the database.
+        /// </summary>
+        /// <param name="id">Id of the BonusEffect to be Deleted.</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> DeleteBonusEffect(int id);
+    }
+}
