58acf6a5d58a19a9b11a793c99f8f741d482afea
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-06 00:11:58 +0800

    Add Ornament repository

---
+++ trailblazers-api/trailblazers-api/Repositories/Ornaments/IOrnamentRepository.cs
@@ -0,0 +1,49 @@
+﻿using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Ornaments
+{
+    public interface IOrnamentRepository
+    {
+        /// <summary>
+        /// Create a new Ornament in the Database.
+        /// </summary>
+        /// <param name="ornament">New Ornament to be created.</param>
+        /// <returns>A int Data type which is the Id of the newly created Ornament</returns>
+        Task<int> CreateOrnament(Ornament ornament);
+        /// <summary>
+        /// Gets all Ornament in the databse.
+        /// </summary>
+        /// <returns><IEnumerable<Ornament>></returns>
+        Task<IEnumerable<Ornament>> GetAllOrnaments();
+        /// <summary>
+        /// Gets Ornament in the database by the Id.
+        /// </summary>
+        /// <param name="id">Id of the Ornament to get in the database.</param>
+        /// <returns>A nullable Ornament</returns>
+        Task<Ornament?> GetOrnamentById(int id);
+        /// <summary>
+        /// Gets a Ornament in the databse by Name.
+        /// </summary>
+        /// <param name="name">Name of the Ornament to get.</param>
+        /// <returns>A nullable Ornament</returns>
+        Task<Ornament?> GetOrnamentByName(string name);
+        /// <summary>
+        /// Updates a Ornament in the database.
+        /// </summary>
+        /// <param name="ornament">Updated Ornament</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> UpdateOrnament(Ornament ornament);
+        /// <summary>
+        /// Deletes a Ornament in the database.
+        /// </summary>
+        /// <param name="id">Id of the Ornament to be Deleted.</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> DeleteOrnament(int id);
+    }
+}
---
+++ trailblazers-api/trailblazers-api/Repositories/Ornaments/OrnamentRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using trailblazers_api.Context;
+using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Ornaments
+{
+    public class OrnamentRepository : IOrnamentRepository
+    {
+        private readonly DapperContext _context;
+
+        public OrnamentRepository(DapperContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CreateOrnament(Ornament ornament)
+        {
+            var sql = "INSERT INTO Ornaments (Name, Description, Image) VALUES (@Name, @Description, @Image); " +
+                      "SELECT SCOPE_IDENTITY();";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteScalarAsync<int>(sql, new { ornament.Name, ornament.Description, ornament.Image });
+            }
+        }
+        public async Task<IEnumerable<Ornament>> GetAllOrnaments()
+        {
+            var sql = "SELECT * FROM Ornaments;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QueryAsync<Ornament>(sql);
+            }
+        }
+        public async Task<Ornament?> GetOrnamentById(int id)
+        {
+            var sql = "SELECT * FROM Ornaments WHERE Id = @Id;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<Ornament>(sql, new { id });
+            }
+        }
+        public async Task<Ornament?> GetOrnamentByName(string name)
+        {
+            var sql = "SELECT * FROM Ornaments WHERE Name = @Name;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<Ornament>(sql, new { name });
+            }
+        }
+        public async Task<bool> UpdateOrnament(Ornament ornament)
+        {
+            var sql = "UPDATE Ornaments SET Description = @Description WHERE Id = @Id;";
+
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteAsync(sql, new { ornament.Description, ornament.Id }) > 0;
+            }
+        }
+        public Task<bool> DeleteOrnament(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
