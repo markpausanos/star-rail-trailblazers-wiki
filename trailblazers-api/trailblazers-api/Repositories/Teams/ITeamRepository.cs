6e1b2cec549ac8f1d156c097453209c80c3a6527
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-06 00:29:14 +0800

    Add Team Repository

---
+++ trailblazers-api/trailblazers-api/Repositories/Teams/ITeamRepository.cs
@@ -0,0 +1,49 @@
+﻿using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Teams
+{
+    public interface ITeamRepository
+    {
+        /// <summary>
+        /// Create a new Team in the Database.
+        /// </summary>
+        /// <param name="team">New Team to be created.</param>
+        /// <returns>A int Data type which is the Id of the newly created Team</returns>
+        Task<int> CreateTeam(Team team);
+        /// <summary>
+        /// Gets all Team in the databse.
+        /// </summary>
+        /// <returns><IEnumerable<Team>></returns>
+        Task<IEnumerable<Team>> GetAllTeams();
+        /// <summary>
+        /// Gets Team in the database by the Id.
+        /// </summary>
+        /// <param name="id">Id of the Team to get in the database.</param>
+        /// <returns>A nullable Team</returns>
+        Task<Team?> GetTeamById(int id);
+        /// <summary>
+        /// Gets a Team in the databse by Name.
+        /// </summary>
+        /// <param name="name">Name of the Team to get.</param>
+        /// <returns>A nullable Team</returns>
+        Task<Team?> GetTeamByName(string name);
+        /// <summary>
+        /// Updates a Team in the database.
+        /// </summary>
+        /// <param name="team">Updated Team</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> UpdateTeam(Team team);
+        /// <summary>
+        /// Deletes a Team in the database.
+        /// </summary>
+        /// <param name="id">Id of the Team to be Deleted.</param>
+        /// <returns>
+        ///     true : If succesfully.
+        ///     false : If unsuccessful.
+        /// </returns>
+        Task<bool> DeleteTeam(int id);
+    }
+}
---
+++ trailblazers-api/trailblazers-api/Repositories/Teams/TeamRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using trailblazers_api.Context;
+using trailblazers_api.Models;
+
+namespace trailblazers_api.Repositories.Teams
+{
+    public class TeamRepository
+    {
+        private readonly DapperContext _context;
+
+        public TeamRepository(DapperContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CreateTeam(Team team)
+        {
+            var sql = "INSERT INTO Teams (Name, Description, Image) VALUES (@Name, @Description, @Image); " +
+                      "SELECT SCOPE_IDENTITY();";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteScalarAsync<int>(sql, new { team.Name, team.Description, team.Image });
+            }
+        }
+        public async Task<IEnumerable<Team>> GetAllTeams()
+        {
+            var sql = "SELECT * FROM Teams;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QueryAsync<Team>(sql);
+            }
+        }
+        public async Task<Team?> GetTeamById(int id)
+        {
+            var sql = "SELECT * FROM Teams WHERE Id = @Id;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<Team>(sql, new { id });
+            }
+        }
+        public async Task<Team?> GetTeamByName(string name)
+        {
+            var sql = "SELECT * FROM Teams WHERE Name = @Name;";
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.QuerySingleOrDefaultAsync<Team>(sql, new { name });
+            }
+        }
+        public async Task<bool> UpdateTeam(Team team)
+        {
+            var sql = "UPDATE Teams SET Description = @Description WHERE Id = @Id;";
+
+
+            using (var con = _context.CreateConnection())
+            {
+                return await con.ExecuteAsync(sql, new { team.Description, team.Id }) > 0;
+            }
+        }
+        public Task<bool> DeleteTeam(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
