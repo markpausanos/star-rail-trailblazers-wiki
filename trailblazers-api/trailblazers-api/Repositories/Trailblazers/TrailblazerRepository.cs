e04ad914b5ebd55e93c5779b33af664f1498ddb7
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-06 22:18:06 +0800

    Update Trailblazer repository

--- trailblazers-api/trailblazers-api/Repositories/Trailblazers/TrailblazerRepository.cs
+++ trailblazers-api/trailblazers-api/Repositories/Trailblazers/TrailblazerRepository.cs
@@ -1,67 +1,67 @@
-﻿using Dapper;
-using trailblazers_api.Context;
-using trailblazers_api.Models;
+﻿    using Dapper;
+    using trailblazers_api.Context;
+    using trailblazers_api.Models;
 
-namespace trailblazers_api.Repositories.Trailblazers
-{
-    public class TrailblazerRepository : ITrailblazersRepository
+    namespace trailblazers_api.Repositories.Trailblazers
     {
-        private readonly DapperContext _context;
-
-        public TrailblazerRepository(DapperContext context)
+        public class TrailblazerRepository : ITrailblazersRepository
         {
-            _context = context;
-        }
-        public async Task<int> CreateTrailblazer(Trailblazer trailblazer)
-        {
-            var sql = "INSERT INTO Trailblazers (Name, Description, Image) VALUES (@Name, @Description, @Image); " +
-                      "SELECT SCOPE_IDENTITY();";
+            private readonly DapperContext _context;
 
-            using (var con = _context.CreateConnection())
+            public TrailblazerRepository(DapperContext context)
             {
-                return await con.ExecuteScalarAsync<int>(sql, new { trailblazer.Name, trailblazer.Description, trailblazer.Image });
+                _context = context;
             }
-        }
-        public async Task<IEnumerable<Trailblazer>> GetAllTrailblazers()
-        {
-            var sql = "SELECT * FROM Trailblazers;";
-
-            using (var con = _context.CreateConnection())
+            public async Task<int> CreateTrailblazer(Trailblazer trailblazer)
             {
-                return await con.QueryAsync<Trailblazer>(sql);
-            }
-        }
-        public async Task<Trailblazer?> GetTrailblazerById(int id)
-        {
-            var sql = "SELECT * FROM Trailblazers WHERE Id = @Id;";
+                var sql = "INSERT INTO Trailblazers (Name, Description, Image) VALUES (@Name, @Description, @Image); " +
+                          "SELECT SCOPE_IDENTITY();";
 
-            using (var con = _context.CreateConnection())
+                using (var con = _context.CreateConnection())
+                {
+                    return await con.ExecuteScalarAsync<int>(sql, new { trailblazer.Name, trailblazer.Description, trailblazer.Image });
+                }
+            }
+            public async Task<IEnumerable<Trailblazer>> GetAllTrailblazers()
             {
-                return await con.QuerySingleOrDefaultAsync<Trailblazer>(sql, new { id });
-            }
-        }
-        public async Task<Trailblazer?> GetTrailblazerByName(string name)
-        {
-            var sql = "SELECT * FROM Trailblazers WHERE Name = @Name;";
+                var sql = "SELECT * FROM Trailblazers;";
 
-            using (var con = _context.CreateConnection())
+                using (var con = _context.CreateConnection())
+                {
+                    return await con.QueryAsync<Trailblazer>(sql);
+                }
+            }
+            public async Task<Trailblazer?> GetTrailblazerById(int id)
             {
-                return await con.QuerySingleOrDefaultAsync<Trailblazer>(sql, new { name });
+                var sql = "SELECT * FROM Trailblazers WHERE Id = @Id;";
+
+                using (var con = _context.CreateConnection())
+                {
+                    return await con.QuerySingleOrDefaultAsync<Trailblazer>(sql, new { id });
+                }
             }
-        }
-        public async Task<bool> UpdateTrailblazer(Trailblazer trailblazer)
-        {
-            var sql = "UPDATE Trailblazers SET Description = @Description WHERE Id = @Id;";
-
-
-            using (var con = _context.CreateConnection())
+            public async Task<Trailblazer?> GetTrailblazerByName(string name)
             {
-                return await con.ExecuteAsync(sql, new { trailblazer.Description, trailblazer.Id }) > 0;
+                var sql = "SELECT * FROM Trailblazers WHERE Name = @Name;";
+
+                using (var con = _context.CreateConnection())
+                {
+                    return await con.QuerySingleOrDefaultAsync<Trailblazer>(sql, new { name });
+                }
             }
-        }
-        public Task<bool> DeleteTrailblazer(int id)
-        {
-            throw new NotImplementedException();
+            public async Task<bool> UpdateTrailblazer(Trailblazer trailblazer)
+            {
+                var sql = "UPDATE Trailblazers SET Description = @Description WHERE Id = @Id;";
+
+
+                using (var con = _context.CreateConnection())
+                {
+                    return await con.ExecuteAsync(sql, new { trailblazer.Description, trailblazer.Id }) > 0;
+                }
+            }
+            public Task<bool> DeleteTrailblazer(int id)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
-}
