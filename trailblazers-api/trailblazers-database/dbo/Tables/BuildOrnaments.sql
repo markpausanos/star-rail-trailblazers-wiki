030813e7bba0c4959b42b16e0e1ea2d644513d9b
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-06 14:11:58 +0800

    Update Build to Builds and add MM relationship tables

---
+++ trailblazers-api/trailblazers-database/dbo/Tables/BuildOrnaments.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[BuildOrnament]
+(
+    [BuildId] INT NOT NULL,
+    [OrnamentId] INT NOT NULL,
+    CONSTRAINT [PK_BuildOrnament] PRIMARY KEY ([BuildId], [OrnamentId]),
+    CONSTRAINT [FK_BuildOrnament_Build] FOREIGN KEY ([BuildId]) REFERENCES [dbo].[Build]([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_BuildOrnament_Ornament] FOREIGN KEY ([OrnamentId]) REFERENCES [dbo].[Ornament]([Id]) ON DELETE CASCADE
+);
