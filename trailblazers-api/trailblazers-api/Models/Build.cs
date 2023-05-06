001b3222fff9669d1bcc07f689ec03fc3a02e569
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-06 22:17:30 +0800

    Update Build model

--- trailblazers-api/trailblazers-api/Models/Build.cs
+++ trailblazers-api/trailblazers-api/Models/Build.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public User? User { get; set; }
         public Trailblazer? Trailblazer { get; set; }
+        public Lightcone? Lightcone { get; set; }
         public List<Relic> Relics { get; set; } = new List<Relic>();
         public List<Ornament> Ornaments { get; set; } = new List<Ornament>();
     }
