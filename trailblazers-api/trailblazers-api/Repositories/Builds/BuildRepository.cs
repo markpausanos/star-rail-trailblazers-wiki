4fff14f8b9ba2ee6e0a0fd61316ab9f980a48958
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-06 22:17:12 +0800

    Remove unnecessary  methods in  BuildRepository

--- trailblazers-api/trailblazers-api/Repositories/Builds/BuildRepository.cs
+++ trailblazers-api/trailblazers-api/Repositories/Builds/BuildRepository.cs
@@ -22,40 +22,53 @@ namespace trailblazers_api.Repositories.Builds
                 return await con.ExecuteScalarAsync<int>(sql, new { UserId = build.User!.Id, TrailblazerId = build.Trailblazer!.Id });
             }
         }
-        public async Task<IEnumerable<Build>> GetAllBuilds()
-        {
-            var sql = "SELECT * FROM Builds;";
-
-            using (var con = _context.CreateConnection())
-            {
-                return await con.QueryAsync<Build>(sql);
-            }
-        }
         public async Task<Build?> GetBuildById(int id)
         {
-            var sql = "SELECT * FROM Builds WHERE Id = @Id;";
+            var sql = "SELECT b.*, u.*, t.*, l.*, r.*, o.* " +
+                        "FROM Builds b " +
+                        "LEFT JOIN [User] u ON b.UserId = u.Id " +
+                        "LEFT JOIN Trailblazer t ON b.TrailblazerId = t.Id " +
+                        "LEFT JOIN Lightcone l ON b.LightconeId = l.Id " +
+                        "LEFT JOIN BuildRelics br ON b.Id = br.BuildId " +
+                        "LEFT JOIN Relics r ON br.RelicId = r.Id " +
+                        "LEFT JOIN BuildOrnaments bo ON b.Id = bo.BuildId " +
+                        "LEFT JOIN Ornaments o ON bo.OrnamentId = o.Id " +
+                        "WHERE b.Id = @Id AND b.IsDeleted = 0;";
 
             using (var con = _context.CreateConnection())
             {
-                return await con.QuerySingleOrDefaultAsync<Build>(sql, new { id });
-            }
-        }
-        public async Task<Build?> GetBuildByName(string name)
-        {
-            var sql = "SELECT * FROM Builds WHERE Name = @Name;";
+                var buildDict = new Dictionary<int, Build>();
+                var build = await con.QueryAsync<Build, User, Trailblazer, Lightcone, Relic, Ornament, Build>(
+                    sql,
+                    (build, user, trailblazer, lightcone, relic, ornament) =>
+                    {
+                        if (!buildDict.TryGetValue(build.Id, out var currentBuild))
+                        {
+                            currentBuild = build;
+                            currentBuild.User = user;
+                            currentBuild.Trailblazer = trailblazer;
+                            currentBuild.Lightcone = lightcone;
+                            currentBuild.Relics = new List<Relic>();
+                            currentBuild.Ornaments = new List<Ornament>();
+                            buildDict.Add(currentBuild.Id, currentBuild);
+                        }
 
-            using (var con = _context.CreateConnection())
-            {
-                return await con.QuerySingleOrDefaultAsync<Build>(sql, new { name });
+                        if (relic != null && !currentBuild.Relics.Contains(relic))
+                        {
+                            currentBuild.Relics.Add(relic);
+                        }
+
+                        if (ornament != null && !currentBuild.Ornaments.Contains(ornament))
+                        {
+                            currentBuild.Ornaments.Add(ornament);
+                        }
+
+                        return currentBuild;
+                    },
+                    new { id });
+
+                return build.FirstOrDefault();
             }
-        }
-        public Task<bool> UpdateBuild(Build build)
-        {
-            throw new NotImplementedException();
-        }
-        public Task<bool> DeleteBuild(int id)
-        {
-            throw new NotImplementedException();
         }
     }
 }
