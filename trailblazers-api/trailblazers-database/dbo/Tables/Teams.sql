174179ea629633b03dcb0505591c700bbf2bffad
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-06 23:26:53 +0800

    Add TeamLikes and update Team tables

--- trailblazers-api/trailblazers-database/dbo/Tables/Teams.sql
+++ trailblazers-api/trailblazers-database/dbo/Tables/Teams.sql
@@ -3,5 +3,6 @@
     [Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     [Name] NVARCHAR(MAX) NULL,
     [UserId] INT NULL,
+    [IsDeleted] BIT NOT NULL DEFAULT 0,
     CONSTRAINT [FK_Team_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User]([Id]) ON DELETE CASCADE
 );
