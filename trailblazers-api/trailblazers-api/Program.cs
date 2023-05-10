4b5903cd1067691fa3e3641e13b3675c62a980f0
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-10 23:44:02 +0800

    Initialize Jwt Authentication

--- trailblazers-api/trailblazers-api/Program.cs
+++ trailblazers-api/trailblazers-api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using trailblazers_api.Context;
 using trailblazers_api.Repositories.Builds;
 using trailblazers_api.Repositories.Eidolons;
@@ -13,6 +16,7 @@ using trailblazers_api.Repositories.Traces;
 using trailblazers_api.Repositories.Trailblazers;
 using trailblazers_api.Repositories.Users;
 using trailblazers_api.Services.Elements;
+using trailblazers_api.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,17 +37,31 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
+app.UseAuthentication();
 app.Run();
 
 void ConfigureServices(IServiceCollection services)
 {
+    var configuration = builder.Configuration;
+    var jwtSettings = configuration.GetSection("JwtSettings").Get<JwtSettings>();
+    services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidAudience = jwtSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key!))
+            };
+        });
+
     services.AddTransient<DapperContext>();
 
     services.AddScoped<IElementService, ElementService>();
