06754bdfc7754dd3b4b78043d4b096ed5f23fe81
Hareram Chua <hareram.chua@cit.edu>
2023-05-10 14:02:28 +0800

    Add login page with its design

---
+++ trailblazers-react/src/Login-Signup/Login.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+export const Login = (props) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username)
+    }
+    
+    return (
+        <div className="background-image">
+            <div className="container1">
+                <h2>Login</h2>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="username">Username</label>
+                    <input value ={username} onChange={(e) => setUsername(e.target.value)}type="username" placeholder="Enter username" id="usernameid" name="username" />
+                    <br />
+                    <label htmlFor="password">Password</label>
+                    <input value ={password} onChange={(e) => setPassword(e.target.value)}type="password" placeholder="Enter password" id="passwordid" name="password" />
+                    <br />
+                    <button type="submit">Login</button>
+                    <br />
+                </form>
+                <p>New to Trailblazers? <button className="link-button" onClick={() => props.onFormSwitch('signup')}>Create an account.</button></p>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
