5eab9dbe7102b8d60b0c5ae31122b05fdb7382d3
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-10 21:39:02 +0800

    Add code for add scoped all repo

--- trailblazers-api/trailblazers-api/Program.cs
+++ trailblazers-api/trailblazers-api/Program.cs
@@ -2,6 +2,16 @@ using trailblazers_api.Context;
 using trailblazers_api.Repositories.Builds;
 using trailblazers_api.Repositories.Eidolons;
 using trailblazers_api.Repositories.Elements;
+using trailblazers_api.Repositories.Lightcones;
+using trailblazers_api.Repositories.Ornaments;
+using trailblazers_api.Repositories.Paths;
+using trailblazers_api.Repositories.Posts;
+using trailblazers_api.Repositories.Relics;
+using trailblazers_api.Repositories.Skills;
+using trailblazers_api.Repositories.Teams;
+using trailblazers_api.Repositories.Traces;
+using trailblazers_api.Repositories.Trailblazers;
+using trailblazers_api.Repositories.Users;
 using trailblazers_api.Services.Elements;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -41,4 +51,14 @@ void ConfigureServices(IServiceCollection services)
     services.AddScoped<IBuildRepository, BuildRepository>();
     services.AddScoped<IEidolonRepository, EidolonRepository>();
     services.AddScoped<IElementRepository, ElementRepository>();
+    services.AddScoped<ILightconeRepository, LightconeRepository>();
+    services.AddScoped<IOrnamentRepository, OrnamentRepository>();
+    services.AddScoped<IPathSRRepository, PathSRRepository>();
+    services.AddScoped<IPostRepository, PostRepository>();
+    services.AddScoped<IRelicRepository, RelicRepository>();
+    services.AddScoped<ISkillRepository, SkillRepository>();
+    services.AddScoped<ITeamRepository, TeamRepository>();
+    services.AddScoped<ITraceRepository, TraceRepository>();
+    services.AddScoped<ITrailblazersRepository, TrailblazerRepository>();
+    services.AddScoped<IUserRepository, UserRepository>();
 }
