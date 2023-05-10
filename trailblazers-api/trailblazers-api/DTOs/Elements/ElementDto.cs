e2ab8944de1b0b520ea9b612a0edbdbfca4fd8f9
Timothy Janssen Taboada <timothyjanssen.taboada@cit.edu>
2023-05-10 22:37:23 +0800

    Add Dto Documentation

--- trailblazers-api/trailblazers-api/DTOs/Elements/ElementDto.cs
+++ trailblazers-api/trailblazers-api/DTOs/Elements/ElementDto.cs
@@ -1,11 +1,23 @@
 ﻿namespace trailblazers_api.DTOs.Elements
 {
+    /// <summary>
+    /// Element DTO class
+    /// </summary>
     public class ElementDto
     {
+        /// <summary>
+        /// Id of the Element
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Name of the Element
+        /// </summary>
         public string? Name { get; set; }
 
+        /// <summary>
+        /// Directory path of the Element image
+        /// </summary>
         public string? Image { get; set; }
     }
 }
--- trailblazers-api/trailblazers-api/DTOs/Elements/ElementUpdateDto.cs
+++ trailblazers-api/trailblazers-api/DTOs/Elements/ElementUpdateDto.cs
@@ -2,14 +2,26 @@
 
 namespace trailblazers_api.DTOs.Elements
 {
+    /// <summary>
+    /// Element update DTO class
+    /// </summary>
     public class ElementUpdateDto
     {
+        /// <summary>
+        /// Id of the Element
+        /// </summary>
         [Required(ErrorMessage = "Id is a required field")]
         public int Id { get; set; }
 
+        /// <summary>
+        /// Name of the Element
+        /// </summary>
         [Required(ErrorMessage = "Name is a required field")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Directory path of the Element image
+        /// </summary>
         [Required(ErrorMessage = "Image is a required field")]
         public string Image { get; set; }
     }
