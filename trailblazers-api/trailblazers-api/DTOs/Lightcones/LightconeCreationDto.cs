723425e9f440d58d008ba44cdf3cd2878d8cef51
Timothy Janssen Taboada <timothyjanssen.taboada@cit.edu>
2023-05-11 20:00:07 +0800

    Added Lightcone dtos

---
+++ trailblazers-api/trailblazers-api/DTOs/Lightcones/LightconeCreationDto.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace trailblazers_api.DTOs.Lightcones
+{
+    /// <summary>
+    /// Lightcone DTO class
+    /// </summary>
+    public class LightconeCreationDto
+    {
+        /// <summary>
+        /// Title of the Lightcone
+        /// </summary>
+        [Required(ErrorMessage = "Title is a required field")]
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// Name of the Lightcone
+        /// </summary>
+        [Required(ErrorMessage = "Name is a required field")]
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Description of the Lightcone
+        /// </summary>
+        [Required(ErrorMessage = "Description is a required field")]
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Directory path of the Lightcone image
+        /// </summary>
+        [Required(ErrorMessage = "Image is a required field")]
+        public string? Image { get; set; }
+
+        /// <summary>
+        /// Rarity of the Lightcone
+        /// </summary>
+        [Required(ErrorMessage = "Rarity is a required field")]
+        public int Rarity { get; set; }
+
+        /// <summary>
+        /// BaseAtk stat of the Lightcone
+        /// </summary>
+        [Required(ErrorMessage = "BaseHp is a required field")]
+        public int BaseHp { get; set; }
+
+        /// <summary>
+        /// BaseAtk stat of the Lightcone
+        /// </summary>
+        [Required(ErrorMessage = "BaseAtk is a required field")]
+        public int BaseAtk { get; set; }
+
+        /// <summary>
+        /// BaseDef stat of the Lightcone
+        /// </summary>
+        [Required(ErrorMessage = "BaseDef is a required field")]
+        public int BaseDef { get; set; }
+    }
+}
