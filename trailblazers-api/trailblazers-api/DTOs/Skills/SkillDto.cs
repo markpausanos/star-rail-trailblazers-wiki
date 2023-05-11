1a8ab2638e3ef9a7ce07faf22c54d4c4b5720901
Timothy Janssen Taboada <timothyjanssen.taboada@cit.edu>
2023-05-11 23:58:07 +0800

    Added Skill dtos

---
+++ trailblazers-api/trailblazers-api/DTOs/Skills/SkillDto.cs
@@ -0,0 +1,43 @@
+﻿namespace trailblazers_api.DTOs.Skills
+{
+    /// <summary>
+    /// Skill DTO class
+    /// </summary>
+    public class SkillDto
+    {
+        /// <summary>
+        /// Id of the Skill
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Title of the Skill
+        /// </summary>
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// Name of the Skill
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Description of the Skill
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Image of the Skill
+        /// </summary>
+        public string? Image { get; set; }
+
+        /// <summary>
+        /// Type of the Skill
+        /// </summary>
+        public string? Type { get; set; }
+
+        /// <summary>
+        /// Id of the Skill Trailblazer
+        /// </summary>
+        public int TrailblazerId { get; set; }
+    }
+}
