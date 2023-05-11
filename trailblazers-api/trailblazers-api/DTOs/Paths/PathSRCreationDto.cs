7b1754c64a87766640a3f7575dd0aad877f972bf
Timothy Janssen Taboada <timothyjanssen.taboada@cit.edu>
2023-05-11 11:24:35 +0800

    Added Path DTOs

---
+++ trailblazers-api/trailblazers-api/DTOs/Paths/PathSRCreationDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace trailblazers_api.DTOs.Paths
+{
+    /// <summary>
+    /// Path creation DTO class
+    /// </summary>
+    public class PathSRCreationDto
+    {
+        /// <summary>
+        /// Name of the Path
+        /// </summary>
+        [Required(ErrorMessage = "Name is a required field")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Directory path of the Path image
+        /// </summary>
+        [Required(ErrorMessage = "Image is a required field")]
+        public string Image { get; set; }
+    }
+}
---
+++ trailblazers-api/trailblazers-api/DTOs/Paths/PathSRUpdateDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace trailblazers_api.DTOs.Paths
+{
+    /// <summary>
+    /// Path update DTO class
+    /// </summary>
+    public class PathSRUpdateDto
+    {
+        /// <summary>
+        /// Id of the Path
+        /// </summary>
+        [Required(ErrorMessage = "Id is a required field")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the Path
+        /// </summary>
+        [Required(ErrorMessage = "Name is a required field")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Directory path of the Path image
+        /// </summary>
+        [Required(ErrorMessage = "Image is a required field")]
+        public string Image { get; set; }
+    }
+}
