9fa4161564aad41fe0a5992289584cbef0aeb590
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2023-05-13 10:23:01 +0800

    Add navigate

--- trailblazers-react/src/App.js
+++ trailblazers-react/src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import { Routes, Route } from 'react-router';
 import Login from './Login-Signup/Login';
 import Dashboard from './CharacterListDashboard/Dashboard';
+import SignUp from './Login-Signup/SignUp';
+
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Login />}></Route>
+      <Route path="sign-up" element={<SignUp />}></Route>
       <Route path="dashboard" element={<Dashboard />}></Route>
     </Routes>
   );
