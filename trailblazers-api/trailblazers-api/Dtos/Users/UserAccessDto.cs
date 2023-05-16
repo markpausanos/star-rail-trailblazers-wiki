8c520b46e7eaa8abc525556ad15b66a05b1f3eaf
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-16 23:07:43 +0800

    Add more User dto

---
+++ trailblazers-api/trailblazers-api/Dtos/Users/UserAccessDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace trailblazers_api.Dtos.Users
+{
+    public class UserAccessDto
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ trailblazers-api/trailblazers-api/Dtos/Users/UserUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace trailblazers_api.Dtos.Users
+{
+    public class UserUpdateDto
+    {
+        [Required(ErrorMessage = "Password is required")]
+        [MaxLength(50, ErrorMessage = "Password can have at most 50 characters")]
+        public string? Password { get; set; }
+    }
+}
