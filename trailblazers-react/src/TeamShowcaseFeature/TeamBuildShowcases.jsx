bf97780e37daa3e62b7071ecf99275c283bab4ba
Shane Denney Cuizon <shanedenney.cuizon@cit.edu>
2023-05-17 12:07:57 +0800

    Create Team build showcases page

---
+++ trailblazers-react/src/TeamShowcaseFeature/TeamBuildShowcases.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TeamBuildShowcases.css';
+
+function TeamBuildShowcases() {
+    return (
+        <div></div>
+    );
+  }
+
+export default TeamBuildShowcases;
