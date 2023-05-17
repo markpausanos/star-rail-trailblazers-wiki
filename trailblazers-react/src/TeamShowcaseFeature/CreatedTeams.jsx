bb660cf7ec93e8d5fee237df413256746e261d3f
Shane Denney Cuizon <shanedenney.cuizon@cit.edu>
2023-05-17 12:01:23 +0800

    Add Created Teams page

---
+++ trailblazers-react/src/TeamShowcaseFeature/CreatedTeams.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CreatedTeams.css';
+import Team from './Team'
+
+function CreatedTeams() {
+    return (
+        <div className='teams'><Team/></div>
+
+    );
+  }
+
+export default CreatedTeams;
