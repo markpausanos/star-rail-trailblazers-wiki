f52d128cd729bcbba0a85188a537aab1532209b3
Shane Denney Cuizon <shanedenney.cuizon@cit.edu>
2023-05-17 12:05:11 +0800

    Create User team build post component

---
+++ trailblazers-react/src/TeamShowcaseFeature/UserBuildPost.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './UserBuildPost.css';
+
+function UserBuildPost() {
+  
+    return (
+      <div className='postbase'>Sample User Post</div>
+    );
+  }
+
+export default UserBuildPost;
