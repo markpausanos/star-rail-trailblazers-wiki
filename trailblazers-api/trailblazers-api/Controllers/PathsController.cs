374e5b51082c6dacc85516a0ddac0adce2e0b64f
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-19 15:22:35 +0800

    Update Path controller

--- trailblazers-api/trailblazers-api/Controllers/PathsController.cs
+++ trailblazers-api/trailblazers-api/Controllers/PathsController.cs
@@ -1,55 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using trailblazers_api.DTOs.Paths;
+using trailblazers_api.DTOs.Relics;
 using trailblazers_api.Services.Paths;
+using Microsoft.IdentityModel.Tokens;
 
 namespace trailblazers_api.Controllers
 {
+    /// <summary>
+    /// Controller for managing Paths.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class PathsController : ControllerBase
     {
         private readonly ILogger<PathsController> _logger;
-        private readonly IPathSRService _service;
+        private readonly IPathSRService _pathService;
 
         public PathsController(ILogger<PathsController> logger, IPathSRService service)
         {
             _logger = logger;
-            _service = service;
+            _pathService = service;
         }
 
         /// <summary>
-        /// Creates a new Path.
+        /// Creates a new PathSR.
         /// </summary>
-        /// <param name="path">The Path to be created.</param>
-        /// <returns>The the newly created Path.</returns>
-        /// <remarks>
-        /// Sample request:
-        ///
-        /// POST /api/PathSR
-        /// {
-        /// "name": "Abundance",
-        /// "image": "https://example.com/abundance.png"
-        /// }
-        ///
-        /// </remarks>
-        /// <response code="201">The Path was successfully created.</response>
-        /// <response code="204">No content.</response>
-        /// <response code="400">Invalid request.</response>
-        /// <response code="500">An internal server error occurred.</response>
+        /// <param name="path">The PathSR creation data.</param>
+        /// <returns>The created PathSR.</returns>
         [HttpPost(Name = "CreatePath")]
         [Consumes("application/json")]
         [Produces("application/json")]
+        [Authorize(Roles = "A")]
         [ProducesResponseType(typeof(PathSRDto), StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> CreatePathSR([FromForm] PathSRCreationDto path)
+        public async Task<IActionResult> CreatePathSR(PathSRCreationDto path)
         {
             try
             {
-                var newPathId = await _service.CreatePathSR(path);
-                var newPath = await _service.GetPathSRById(newPathId);
+                var newPath = await _pathService.CreatePathSR(path);
+
+                if (newPath == null)
+                {
+                    return BadRequest("Path cannot be created.");
+                }
 
                 return CreatedAtRoute("GetPathById", new { id = newPath.Id }, newPath);
             }
@@ -61,29 +61,27 @@ namespace trailblazers_api.Controllers
         }
 
         /// <summary>
-        /// Gets all Paths in the database.
+        /// Gets all PathSRs.
         /// </summary>
-        /// <returns>IEnumerable with all Paths.</returns>
-        /// <response code="200">The Paths were successfully retrieved.</response>
-        /// <response code="204">No content.</response>
-        /// <response code="400">Invalid request.</response>
-        /// <response code="500">An internal server error occurred.</response>
+        /// <param name="name">Optional. Filter paths by name.</param>
+        /// <returns>The list of PathSRs.</returns>
         [HttpGet(Name = "GetAllPaths")]
         [Produces("application/json")]
         [ProducesResponseType(typeof(IEnumerable<PathSRDto>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetAllPathSRs()
+        public async Task<IActionResult> GetAllPathSRs([FromQuery] string? name)
         {
             try
             {
-                var paths = await _service.GetAllPathSRs();
+                var paths = string.IsNullOrEmpty(name) ? await _pathService.GetAllPathSRs() :
+                    new List<PathSRDto> { await _pathService.GetPathSRByName(name) };
 
                 if (paths.IsNullOrEmpty())
                 {
                     return NoContent();
                 }
+
                 return Ok(paths);
             }
             catch (Exception e)
@@ -94,30 +92,26 @@ namespace trailblazers_api.Controllers
         }
 
         /// <summary>
-        /// Gets Path with the given Id from the database.
+        /// Gets a PathSR by ID.
         /// </summary>
-        /// <param name="id">Id of Path to be retrieved.</param>
-        /// <returns>Path with Id.</returns>
-        /// <response code="200">The Path was successfully retrieved.</response>
-        /// <response code="204">No content.</response>
-        /// <response code="400">The Path details are invalid.</response>
-        /// <response code="500">An internal server error occurred.</response>
+        /// <param name="id">The ID of the PathSR to retrieve.</param>
+        /// <returns>The PathSR with the specified ID.</returns>
         [HttpGet("{id}", Name = "GetPathById")]
         [Produces("application/json")]
         [ProducesResponseType(typeof(PathSRDto), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetPathSRById(int id)
         {
             try
             {
-                var path = await _service.GetPathSRById(id);
+                var path = await _pathService.GetPathSRById(id);
 
                 if (path == null)
                 {
                     return NoContent();
                 }
+
                 return Ok(path);
             }
             catch (Exception e)
@@ -128,76 +122,39 @@ namespace trailblazers_api.Controllers
         }
 
         /// <summary>
-        /// Gets Path with the given Name from the database.
+        /// Updates a PathSR.
         /// </summary>
-        /// <param name="id">Name of Path to be retrieved.</param>
-        /// <returns>Path with Name.</returns>
-        /// <response code="200">The Path was successfully retrieved.</response>
-        /// <response code="204">No content.</response>
-        /// <response code="400">The Path details are invalid.</response>
-        /// <response code="500">An internal server error occurred.</response>
-        [HttpGet("{name}", Name = "GetPathByName")]
-        [Produces("application/json")]
-        [ProducesResponseType(typeof(PathSRDto), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetPathSRByName(string name)
-        {
-            try
-            {
-                var path = await _service.GetPathSRByName(name);
-
-                if (path == null)
-                {
-                    return NoContent();
-                }
-                return Ok(path);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e.Message);
-                return StatusCode(500, "An error occurred while retrieving the Path.");
-            }
-        }
-
-        /// <summary>
-        /// Updates Path with given data.
-        /// </summary>
-        /// <param name="updatePath">Data to update to the Path.</param>
-        /// <returns>True if the updating was successful, false otherwise.</returns>
-        /// <remarks>
-        /// Sample request:
-        ///
-        /// PUT /api/PathSR
-        /// {
-        /// "id": 2
-        /// "name": "Abundance",
-        /// "image": "https://example.com/abundance.png"
-        /// }
-        /// </remarks>
-        /// <response code="200">The Path was successfully updated.</response>
-        /// <response code="404">The Path was not found.</response>
-        /// <response code="500">An internal server error occurred.</response>
+        /// <param name="id">The ID of the PathSR to update.</param>
+        /// <param name="newPath">The updated PathSR data.</param>
+        /// <returns>The updated PathSR.</returns>
         [HttpPut(Name = "UpdatePath")]
         [Consumes("application/json")]
         [Produces("application/json")]
         [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> UpdatePathSR([FromForm] PathSRUpdateDto updatePath)
+        public async Task<IActionResult> UpdatePathSR(int id, [FromBody] PathSRUpdateDto newPath)
         {
             try
             {
-                int id = updatePath.Id;
-                var path = await _service.GetPathSRById(id);
+                var path = await _pathService.GetPathSRById(id);
+
                 if (path == null)
                 {
                     return NotFound($"Path with ID = {id} does not exist.");
                 }
 
-                var updatedPath = await _service.UpdatePathSR(updatePath);
-                return Ok(updatedPath);
+                newPath.Name ??= path.Name;
+                newPath.Image ??= path.Image;
+
+                if (await _pathService.UpdatePathSR(id, newPath))
+                {
+                    var updatedPath = await _pathService.GetPathSRById(id);
+
+                    return Ok(updatedPath);
+                }
+
+                return BadRequest();
             }
             catch (Exception e)
             {
@@ -207,36 +164,25 @@ namespace trailblazers_api.Controllers
         }
 
         /// <summary>
-        /// Deletes an Path with the given Id
+        /// Deletes a PathSR by ID.
         /// </summary>
-        /// <param name="id">Id of Path to be deleted</param>
-        /// <returns>True if the deletion was successful, false otherwise.</returns>
-        /// <response code="200">The Path was successfully deleted.</response>
-        /// <response code="400">Invalid request.</response>
-        /// <response code="404">The Path was not found.</response>
-        /// <response code="500">An internal server error occurred.</response>
+        /// <param name="id">The ID of the PathSR to delete.</param>
+        /// <returns>A boolean indicating whether the deletion was successful.</returns>
         [HttpDelete("{id}", Name = "DeletePath")]
         [Produces("application/json")]
         [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> DeletePathSR(int id)
         {
             try
             {
-                var path = await _service.GetPathSRById(id);
-                if (path == null)
+                if (await _pathService.DeletePathSR(id))
                 {
-                    return NotFound($"Path with ID = {id} not found.");
+                    return Ok($"Successfully deleted path with ID {id}.");
                 }
 
-                var isDeleted = await _service.DeletePathSR(id);
-                if (isDeleted)
-                {
-                    return Ok("Successfully deleted.");
-                }
-                return BadRequest($"Path with ID = {id} could not be deleted.");
+                return BadRequest();
             }
             catch (Exception e)
             {
