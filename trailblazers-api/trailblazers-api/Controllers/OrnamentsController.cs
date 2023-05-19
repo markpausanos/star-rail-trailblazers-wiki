0b153369d785ade26e9beec59bc6ef61e56ce166
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-19 23:10:01 +0800

    Add Ornament controller

--- trailblazers-api/trailblazers-api/Controllers/OrnamentsController.cs
+++ trailblazers-api/trailblazers-api/Controllers/OrnamentsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using trailblazers_api.DTOs.Ornaments;
 using trailblazers_api.Services.Ornaments;
@@ -10,47 +11,36 @@ namespace trailblazers_api.Controllers
     public class OrnamentsController : ControllerBase
     {
         private readonly ILogger<OrnamentsController> _logger;
-        private readonly IOrnamentService _service;
+        private readonly IOrnamentService _ornamentService;
 
         public OrnamentsController(ILogger<OrnamentsController> logger, IOrnamentService service)
         {
             _logger = logger;
-            _service = service;
+            _ornamentService = service;
         }
 
         /// <summary>
         /// Creates a new Ornament in the database.
         /// </summary>
         /// <param name="ornament">The Ornament object to be created.</param>
-        /// <returns>An integer that represents the Id of the newly created Ornament.</returns>
-        /// <remarks>
-        /// Sample request:
-        ///
-        /// POST /api/Ornaments
-        /// {
-        /// "name": "Space Station 13",
-        /// "description": "Ningguang Ghanglian",
-        /// "image": "https://example.com/hss.png"
-        /// }
-        ///
-        /// </remarks>
-        /// <response code="201">The Ornament was successfully created.</response>
-        /// <response code="204">No content.</response>
-        /// <response code="400">Invalid request.</response>
-        /// <response code="500">An internal server error occurred.</response>
+        /// <returns>The created Ornament object.</returns>
         [HttpPost(Name = "CreateOrnament")]
         [Consumes("application/json")]
         [Produces("application/json")]
+        [Authorize(Roles = "A")]
         [ProducesResponseType(typeof(OrnamentDto), StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> CreateOrnament([FromForm] OrnamentCreationDto ornament)
+        public async Task<IActionResult> CreateOrnament(OrnamentCreationDto ornament)
         {
             try
             {
-                var newOrnamentId = await _service.CreateOrnament(ornament);
-                var newOrnament = await _service.GetOrnamentById(newOrnamentId);
+                var newOrnament = await _ornamentService.CreateOrnament(ornament);
+
+                if (newOrnament == null)
+                {
+                    return BadRequest("Ornament cannot be created.");
+                }
 
                 return CreatedAtRoute("GetOrnamentById", new { id = newOrnament.Id }, newOrnament);
             }
@@ -64,27 +54,25 @@ namespace trailblazers_api.Controllers
         /// <summary>
         /// Retrieves all Ornament objects from the database.
         /// </summary>
+        /// <param name="name">Optional. Filters the ornaments by name.</param>
         /// <returns>An IEnumerable collection of Ornament objects.</returns>
-        /// <response code="200">The Paths were successfully retrieved.</response>
-        /// <response code="204">No content.</response>
-        /// <response code="400">Invalid request.</response>
-        /// <response code="500">An internal server error occurred.</response>
         [HttpGet(Name = "GetAllOrnaments")]
         [Produces("application/json")]
         [ProducesResponseType(typeof(IEnumerable<OrnamentDto>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetAllOrnaments()
+        public async Task<IActionResult> GetAllOrnaments([FromQuery] string? name)
         {
             try
             {
-                var ornaments = await _service.GetAllOrnaments();
+                var ornaments = string.IsNullOrEmpty(name) ? await _ornamentService.GetAllOrnaments() :
+                    new List<OrnamentDto> {  await _ornamentService.GetOrnamentByName(name) };
 
                 if (ornaments.IsNullOrEmpty())
                 {
                     return NoContent();
                 }
+                
                 return Ok(ornaments);
             }
             catch (Exception e)
@@ -98,61 +86,23 @@ namespace trailblazers_api.Controllers
         /// Retrieves an Ornament object from the database by its Id.
         /// </summary>
         /// <param name="id">The Id of the Ornament to be retrieved.</param>
-        /// <returns>A nullable Ornament object.</returns>
-        /// <response code="200">The Ornament was successfully retrieved.</response>
-        /// <response code="204">No content.</response>
-        /// <response code="400">The Ornament details are invalid.</response>
-        /// <response code="500">An internal server error occurred.</response>
+        /// <returns>The Ornament object.</returns>
         [HttpGet("{id}", Name = "GetOrnamentById")]
         [Produces("application/json")]
         [ProducesResponseType(typeof(OrnamentDto), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetOrnamentById(int id)
         {
             try
             {
-                var ornament = await _service.GetOrnamentById(id);
+                var ornament = await _ornamentService.GetOrnamentById(id);
 
                 if (ornament == null)
                 {
                     return NoContent();
                 }
-                return Ok(ornament);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e.Message);
-                return StatusCode(500, "An error occurred while retrieving the Ornament.");
-            }
-        }
 
-        /// <summary>
-        /// Retrieves an Ornament object from the database by its Name.
-        /// </summary>
-        /// <param name="name">The Name of the Ornament to be retrieved.</param>
-        /// <returns>A nullable Ornament object.</returns>
-        /// <response code="200">The Ornament was successfully retrieved.</response>
-        /// <response code="204">No content.</response>
-        /// <response code="400">The Ornament details are invalid.</response>
-        /// <response code="500">An internal server error occurred.</response>
-        [HttpGet("{name}", Name = "GetOrnamentByName")]
-        [Produces("application/json")]
-        [ProducesResponseType(typeof(OrnamentDto), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetOrnamentByName(string name)
-        {
-            try
-            {
-                var ornament = await _service.GetOrnamentByName(name);
-
-                if (ornament == null)
-                {
-                    return NoContent();
-                }
                 return Ok(ornament);
             }
             catch (Exception e)
@@ -165,42 +115,38 @@ namespace trailblazers_api.Controllers
         /// <summary>
         /// Updates an Ornament object in the database.
         /// </summary>
-        /// <param name="updateOrnament">The updated Ornament object.</param>
-        /// <returns>A boolean value indicating whether the operation was successful.</returns>
-        /// <remarks>
-        /// Sample request:
-        ///
-        /// PUT /api/Ornaments
-        /// {
-        /// "id": 13,
-        /// "name": "Space Station 13",
-        /// "description": "Ningguang Ghanglian",
-        /// "image": "https://example.com/hss.png"
-        /// }
-        /// }
-        /// </remarks>
-        /// <response code="200">The Ornament was successfully updated.</response>
-        /// <response code="404">The Ornament was not found.</response>
-        /// <response code="500">An internal server error occurred.</response>
+        /// <param name="id">The Id of the Ornament to be updated.</param>
+        /// <param name="newOrnament">The updated Ornament object.</param>
+        /// <returns>The updated Ornament object.</returns>
         [HttpPut(Name = "UpdateOrnament")]
         [Consumes("application/json")]
         [Produces("application/json")]
         [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> UpdateOrnament([FromForm] OrnamentUpdateDto updateOrnament)
+        public async Task<IActionResult> UpdateOrnament(int id, [FromBody] OrnamentUpdateDto newOrnament)
         {
             try
             {
-                int id = updateOrnament.Id;
-                var ornament = await _service.GetOrnamentById(id);
+                var ornament = await _ornamentService.GetOrnamentById(id);
+
                 if (ornament == null)
                 {
                     return NotFound($"Ornament with ID = {id} does not exist.");
                 }
 
-                var updatedOrnament = await _service.UpdateOrnament(updateOrnament);
-                return Ok(updatedOrnament);
+                newOrnament.Name ??= ornament.Name;
+                newOrnament.Description ??= ornament.Description;
+                newOrnament.Image ??= ornament.Image;
+
+                if (await _ornamentService.UpdateOrnament(id, newOrnament))
+                {
+                    var updatedOrnament = await _ornamentService.GetOrnamentById(id);
+
+                    return Ok(updatedOrnament);
+                }
+
+                return BadRequest();
             }
             catch (Exception e)
             {
@@ -209,15 +155,11 @@ namespace trailblazers_api.Controllers
             }
         }
 
-        /// <summary>
+        // <summary>
         /// Deletes an Ornament object from the database.
         /// </summary>
         /// <param name="id">The Id of the Ornament to be deleted.</param>
         /// <returns>A boolean value indicating whether the operation was successful.</returns>
-        /// <response code="200">The Ornament was successfully deleted.</response>
-        /// <response code="400">Invalid request.</response>
-        /// <response code="404">The Ornament was not found.</response>
-        /// <response code="500">An internal server error occurred.</response>
         [HttpDelete("{id}", Name = "DeleteOrnament")]
         [Produces("application/json")]
         [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
@@ -228,18 +170,12 @@ namespace trailblazers_api.Controllers
         {
             try
             {
-                var ornament = await _service.GetOrnamentById(id);
-                if (ornament == null)
+                if (await _ornamentService.DeleteOrnament(id))
                 {
-                    return NotFound($"Ornament with ID = {id} not found.");
+                    return Ok($"Successfully deleted ornament with ID {id}.");
                 }
 
-                var isDeleted = await _service.DeleteOrnament(id);
-                if (isDeleted)
-                {
-                    return Ok("Successfully deleted.");
-                }
-                return BadRequest($"Ornament with ID = {id} could not be deleted.");
+                return BadRequest();
             }
             catch (Exception e)
             {
