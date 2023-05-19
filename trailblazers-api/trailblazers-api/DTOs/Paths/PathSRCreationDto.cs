d21fd6d9502c5a86196fe775228595bfa0a8d032
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-19 15:22:22 +0800

    Add PathDtos

--- trailblazers-api/trailblazers-api/DTOs/Paths/PathSRCreationDto.cs
+++ trailblazers-api/trailblazers-api/DTOs/Paths/PathSRCreationDto.cs
@@ -2,21 +2,12 @@
 
 namespace trailblazers_api.DTOs.Paths
 {
-    /// <summary>
-    /// Path creation DTO class
-    /// </summary>
     public class PathSRCreationDto
     {
-        /// <summary>
-        /// Name of the Path
-        /// </summary>
         [Required(ErrorMessage = "Name is a required field")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        /// <summary>
-        /// Directory path of the Path image
-        /// </summary>
         [Required(ErrorMessage = "Image is a required field")]
-        public string Image { get; set; }
+        public string? Image { get; set; }
     }
 }
--- trailblazers-api/trailblazers-api/DTOs/Paths/PathSRDto.cs
+++ trailblazers-api/trailblazers-api/DTOs/Paths/PathSRDto.cs
@@ -1,21 +1,9 @@
 ﻿namespace trailblazers_api.DTOs.Paths
 {
-    /// <summary>
-    /// Path DTO class
-    /// </summary>
     public class PathSRDto
     {
-        /// <summary>
-        /// Id of the Path
-        /// </summary>
         public int Id { get; set; }
-        /// <summary>
-        /// Name of the Path
-        /// </summary>
         public string? Name { get; set; }
-        /// <summary>
-        /// Directory path of the Path image
-        /// </summary>
         public string? Image { get; set; }
     }
 }
