852bf500df21cc0df68d0bac950fdfc4ccb72490
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-19 15:21:50 +0800

    Add PathSR mapper

---
+++ trailblazers-api/trailblazers-api/Mapper/PathSRMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using trailblazers_api.DTOs.Paths;
+using trailblazers_api.Models;
+
+namespace trailblazers_api.Mapper
+{
+    public class PathSRMapping : Profile
+    {
+        public PathSRMapping()
+        {
+            CreateMap<PathSRCreationDto, PathSR>();
+            CreateMap<PathSR, PathSRDto>();
+            CreateMap<PathSRUpdateDto, PathSR>();
+        }
+    }
+}
