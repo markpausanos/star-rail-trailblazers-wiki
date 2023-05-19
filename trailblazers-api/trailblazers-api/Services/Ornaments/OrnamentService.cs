658b3e04dc8493796221a3746b0310db86da6143
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-19 23:08:48 +0800

    Update OrnamentService

--- trailblazers-api/trailblazers-api/Services/Ornaments/OrnamentService.cs
+++ trailblazers-api/trailblazers-api/Services/Ornaments/OrnamentService.cs
@@ -1,4 +1,5 @@
-﻿using trailblazers_api.DTOs.Ornaments;
+﻿using AutoMapper;
+using trailblazers_api.DTOs.Ornaments;
 using trailblazers_api.Models;
 using trailblazers_api.Repositories.Ornaments;
 
@@ -6,82 +7,55 @@ namespace trailblazers_api.Services.Ornaments
 {
     public class OrnamentService : IOrnamentService
     {
-        private readonly IOrnamentRepository _repository;
+        private readonly IOrnamentRepository _ornamentRepository;
+        private readonly IMapper _mapper;
 
-        public OrnamentService(IOrnamentRepository repository)
+        public OrnamentService(IOrnamentRepository repository, IMapper mapper)
         {
-            _repository = repository;
+            _ornamentRepository = repository;
+            _mapper = mapper;
         }
 
-        public async Task<int> CreateOrnament(OrnamentCreationDto ornament)
+        public async Task<OrnamentDto?> CreateOrnament(OrnamentCreationDto newOrnament)
         {
-            var ornamentModel = new Ornament
-            {
-                Name = ornament.Name,
-                Description = ornament.Description,
-                Image = ornament.Image
-            };
+            var ornamentToCreate = _mapper.Map<Ornament>(newOrnament);
 
-            return ornamentModel.Id = await _repository.CreateOrnament(ornamentModel);
+            var newlyCreatedOrnament = await _ornamentRepository.GetOrnamentById(await _ornamentRepository.CreateOrnament(ornamentToCreate));
+            return _mapper.Map<OrnamentDto>(newlyCreatedOrnament);
         }
 
         public async Task<IEnumerable<OrnamentDto>> GetAllOrnaments()
         {
-            var ornaments = await _repository.GetAllOrnaments();
-            if (ornaments == null) return null;
+            var ornaments = await _ornamentRepository.GetAllOrnaments();
 
-            return ornaments.Select(ornament => new OrnamentDto
-            {
-                Id = ornament.Id,
-                Name = ornament.Name,
-                Description = ornament.Description,
-                Image = ornament.Image
-            });
+            return ornaments.Select(ornament => _mapper.Map<OrnamentDto>(ornament));
         }
 
         public async Task<OrnamentDto?> GetOrnamentById(int id)
         {
-            var ornament = await _repository.GetOrnamentById(id);
-            if (ornament == null) return null;
+            var ornament = await _ornamentRepository.GetOrnamentById(id);
 
-            return new OrnamentDto
-            {
-                Id = ornament.Id,
-                Name = ornament.Name,
-                Description = ornament.Description,
-                Image = ornament.Image
-            };
+            return ornament == null ? null : _mapper.Map<OrnamentDto>(ornament);
         }
 
         public async Task<OrnamentDto?> GetOrnamentByName(string name)
         {
-            var ornament = await _repository.GetOrnamentByName(name);
-            if (ornament == null) return null;
+            var ornament = await _ornamentRepository.GetOrnamentByName(name);
 
-            return new OrnamentDto
-            {
-                Id = ornament.Id,
-                Name = ornament.Name,
-                Description = ornament.Description,
-                Image = ornament.Image
-            };
+            return ornament == null ? null : _mapper.Map<OrnamentDto>(ornament);
         }
 
-        public async Task<bool> UpdateOrnament(OrnamentUpdateDto ornament)
+        public async Task<bool> UpdateOrnament(int id, OrnamentUpdateDto updatedOrnament)
         {
-            var ornamentModel = new Ornament
-            {
-                Id = ornament.Id,
-                Name = ornament.Name,
-                Description = ornament.Description,
-                Image = ornament.Image
-            };
-            return await _repository.UpdateOrnament(ornamentModel);
+            var ornamentToUpdate = _mapper.Map<Ornament>(updatedOrnament);
+            ornamentToUpdate.Id = id;
+
+            return await _ornamentRepository.UpdateOrnament(ornamentToUpdate);
         }
 
         public async Task<bool> DeleteOrnament(int id)
         {
-            return await _repository.DeleteOrnament(id);
+            return await _ornamentRepository.DeleteOrnament(id);
         }
     }
 }
