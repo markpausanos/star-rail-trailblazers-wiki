99afa8ec5c8e6948ca50e01dc0de5f98ba8afb32
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-20 21:40:50 +0800

    Fix Users

--- trailblazers-api/trailblazers-db/dbo/Tables/Users.sql
+++ trailblazers-api/trailblazers-db/dbo/Tables/Users.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[User] (
     [Id]        INT IDENTITY (1, 1) NOT NULL,
     [Name]      NVARCHAR (100) NOT NULL,
-    [Password]  NVARCHAR (MAX) NULL,
-    [UserType]  CHAR (1) NOT NULL,
+    [Password]  NVARCHAR (100) NULL,
+    [UserType]  CHAR (1) NOT NULL DEFAULT 'U',
     [IsDeleted] BIT NOT NULL DEFAULT 0,
     PRIMARY KEY CLUSTERED ([Id] ASC),
     UNIQUE NONCLUSTERED ([Name] ASC)
