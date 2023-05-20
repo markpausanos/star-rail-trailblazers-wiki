b93e1f6ec350f35ae026a9805e3c7188a4a1e884
Shane Denney Cuizon <shanedenney.cuizon@cit.edu>
2023-05-20 09:18:34 +0800

    create route for teamshowcase page

--- trailblazers-react/src/App.js
+++ trailblazers-react/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router';
 import Login from './Login-Signup/Login';
 import Dashboard from './CharacterListDashboard/Dashboard';
 import SignUp from './Login-Signup/SignUp';
+import TeamShowcase from './TeamShowcaseFeature/TeamShowcase';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' element={<Login />}></Route>
       <Route path="sign-up" element={<SignUp />}></Route>
       <Route path="dashboard" element={<Dashboard />}></Route>
+      <Route path ="teamshowcase" element={<TeamShowcase/>}/>
     </Routes>
   );
 }
