7ce949ac90f68d60cbf69658998d67f1b7c17501
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-20 21:19:59 +0800

    Add Lightcone mapper

---
+++ trailblazers-api/trailblazers-api/Mapper/LightconeMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using trailblazers_api.Dtos.Lightcones;
+using trailblazers_api.Models;
+
+namespace trailblazers_api.Mapper
+{
+    public class LightconeMapping : Profile
+    {
+        public LightconeMapping()
+        {
+            CreateMap<LightconeCreationDto, Lightcone>();
+            CreateMap<Lightcone, LightconeDto>();
+            CreateMap<LightconeUpdateDto, Lightcone>();
+        }
+    }
+}
