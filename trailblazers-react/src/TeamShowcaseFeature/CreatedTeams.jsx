104467d034eb3f2f019ce871ccbf3b405a8236be
Shane Denney Cuizon <shanedenney.cuizon@cit.edu>
2023-05-20 09:19:01 +0800

    modify created teams page

--- trailblazers-react/src/TeamShowcaseFeature/CreatedTeams.jsx
+++ trailblazers-react/src/TeamShowcaseFeature/CreatedTeams.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import './CreatedTeams.css';
-import Team from './Team'
 
 function CreatedTeams() {
     return (
-        <div className='teams'><Team/></div>
+        <div className='teams'></div>
 
     );
   }
