ba64a43b4fa3cc7d1944492704361464373396ea
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-20 15:27:33 +0800

    Make int nullable for TrailblazerUpdate

--- trailblazers-api/trailblazers-api/Dtos/Trailblazers/TrailblazerUpdateDto.cs
+++ trailblazers-api/trailblazers-api/Dtos/Trailblazers/TrailblazerUpdateDto.cs
@@ -4,9 +4,9 @@
     {
         public string? Name { get; set; }
         public string? Image { get; set; }
-        public int BaseHp { get; set; }
-        public int BaseAtk { get; set; }
-        public int BaseDef { get; set; }
-        public int BaseSpeed { get; set; }
+        public int? BaseHp { get; set; }
+        public int? BaseAtk { get; set; }
+        public int? BaseDef { get; set; }
+        public int? BaseSpeed { get; set; }
     }
 }
