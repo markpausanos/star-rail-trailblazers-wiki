67ee3e05021ac9ee6c132d95bcbcb0902fab0782
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-20 21:20:14 +0800

    Update User creation dto

--- trailblazers-api/trailblazers-api/Dtos/Users/UserCreationLoginDto.cs
+++ trailblazers-api/trailblazers-api/Dtos/Users/UserCreationLoginDto.cs
@@ -5,10 +5,12 @@ namespace trailblazers_api.Dtos.Users
     public class UserCreationLoginDto
     {
         [Required(ErrorMessage = "Name is required")]
+        [MinLength(5, ErrorMessage = "Name too short")]
         [MaxLength(50, ErrorMessage = "Name can have at most 50 characters")]
         public string? Name { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password too short")]
         [MaxLength(50, ErrorMessage = "Password can have at most 50 characters")]
         public string? Password { get; set; }
     }
