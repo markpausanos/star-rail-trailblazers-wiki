b51fee2fd36965df70ba78529250c66b29aaf311
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-22 03:00:40 +0800

    Add Controller unit tests

---
+++ trailblazers-api/trailblazers-api-tests/Controllers/OrnamentControllerTests.cs
@@ -0,0 +1,324 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using trailblazers_api.Dtos.Ornaments;
+using trailblazers_api.Services.Ornaments;
+using Xunit;
+
+namespace trailblazers_api.Controllers.Tests
+{
+    public class OrnamentsControllerTests
+    {
+        private readonly Mock<ILogger<OrnamentsController>> _loggerMock;
+        private readonly Mock<IOrnamentService> _ornamentServiceMock;
+        private readonly OrnamentsController _controller;
+
+        public OrnamentsControllerTests()
+        {
+            _loggerMock = new Mock<ILogger<OrnamentsController>>();
+            _ornamentServiceMock = new Mock<IOrnamentService>();
+            _controller = new OrnamentsController(_loggerMock.Object, _ornamentServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task CreateOrnament_ValidOrnament_ReturnsCreatedAtRoute()
+        {
+            // Arrange
+            var ornamentCreationDto = new OrnamentCreationDto { Name = "TestName" };
+            var newOrnament = new OrnamentDto { Name = "TestName" };
+            _ornamentServiceMock.Setup(mock => mock.CreateOrnament(ornamentCreationDto)).ReturnsAsync(newOrnament);
+
+            // Act
+            var result = await _controller.CreateOrnament(ornamentCreationDto) as CreatedAtRouteResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status201Created, result!.StatusCode);
+            Assert.Equal("GetOrnamentById", result!.RouteName);
+            Assert.Equal(newOrnament, result!.Value);
+        }
+
+        [Fact]
+        public async Task CreateOrnament_InvalidOrnament_ReturnsBadRequest()
+        {
+            // Arrange
+            var ornamentCreationDto = new OrnamentCreationDto { Name = "TestName" };
+            _ornamentServiceMock.Setup(mock => mock.CreateOrnament(ornamentCreationDto)).ReturnsAsync((OrnamentDto)null!);
+
+            // Act
+            var result = await _controller.CreateOrnament(ornamentCreationDto) as BadRequestObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status400BadRequest, result!.StatusCode);
+            Assert.Equal("Ornament cannot be created.", result!.Value);
+        }
+
+        [Fact]
+        public async Task CreateOrnament_ExceptionThrown_ReturnsInternalServerError()
+        {
+            // Arrange
+            var ornamentCreationDto = new OrnamentCreationDto { Name = "TestName" };
+            _ornamentServiceMock.Setup(mock => mock.CreateOrnament(ornamentCreationDto)).ThrowsAsync(new Exception("Some error message"));
+
+            // Act
+            var result = await _controller.CreateOrnament(ornamentCreationDto) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result!.StatusCode);
+            Assert.Equal("An error occurred while creating the Ornament.", result!.Value);
+        }
+
+        [Fact]
+        public async Task GetAllOrnaments_NoName_ReturnsOkWithOrnaments()
+        {
+            // Arrange
+            string? name = null;
+            var ornaments = new List<OrnamentDto> { new OrnamentDto { Name = "Name" } };
+            _ornamentServiceMock.Setup(mock => mock.GetAllOrnaments()).ReturnsAsync(ornaments);
+
+            // Act
+            var result = await _controller.GetAllOrnaments(name) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status200OK, result!.StatusCode);
+            Assert.Equal(ornaments, result!.Value);
+        }
+
+        [Fact]
+        public async Task GetAllOrnaments_WithName_ReturnsOkWithOrnament()
+        {
+            // Arrange
+            var name = "SomeOrnamentName";
+            var ornament = new OrnamentDto { Name = "TestName" };
+            _ornamentServiceMock.Setup(mock => mock.GetOrnamentByName(name)).ReturnsAsync(ornament);
+
+            // Act
+            var result = await _controller.GetAllOrnaments(name) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status200OK, result!.StatusCode);
+            Assert.Equal(ornament, result!.Value);
+        }
+
+        [Fact]
+        public async Task GetAllOrnaments_NoOrnaments_ReturnsNoContent()
+        {
+            // Arrange
+            string? name = null;
+            _ornamentServiceMock.Setup(mock => mock.GetAllOrnaments()).ReturnsAsync(new List<OrnamentDto>());
+
+            // Act
+            var result = await _controller.GetAllOrnaments(name) as NoContentResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status204NoContent, result!.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAllOrnaments_ExceptionThrown_ReturnsInternalServerError()
+        {
+            // Arrange
+            string? name = null;
+            _ornamentServiceMock.Setup(mock => mock.GetAllOrnaments()).ThrowsAsync(new Exception("Some error message"));
+
+            // Act
+            var result = await _controller.GetAllOrnaments(name) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result!.StatusCode);
+            Assert.Equal("An error occurred while retrieving the Ornaments.", result!.Value);
+        }
+
+        [Fact]
+        public async Task GetOrnamentById_ExistingId_ReturnsOkWithOrnament()
+        {
+            // Arrange
+            var ornamentId = 1;
+            var ornament = new OrnamentDto { Name = "TestName" };
+            _ornamentServiceMock.Setup(mock => mock.GetOrnamentById(ornamentId)).ReturnsAsync(ornament);
+
+            // Act
+            var result = await _controller.GetOrnamentById(ornamentId) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status200OK, result!.StatusCode);
+            Assert.Equal(ornament, result!.Value);
+        }
+
+        [Fact]
+        public async Task GetOrnamentById_NonExistingId_ReturnsNoContent()
+        {
+            // Arrange
+            var ornamentId = 1;
+            _ornamentServiceMock.Setup(mock => mock.GetOrnamentById(ornamentId)).ReturnsAsync((OrnamentDto)null!);
+
+            // Act
+            var result = await _controller.GetOrnamentById(ornamentId) as NoContentResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status204NoContent, result!.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetOrnamentById_ExceptionThrown_ReturnsInternalServerError()
+        {
+            // Arrange
+            var ornamentId = 1;
+            _ornamentServiceMock.Setup(mock => mock.GetOrnamentById(ornamentId)).ThrowsAsync(new Exception("Some error message"));
+
+            // Act
+            var result = await _controller.GetOrnamentById(ornamentId) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result!.StatusCode);
+            Assert.Equal("An error occurred while retrieving the Ornament.", result!.Value);
+        }
+
+        [Fact]
+        public async Task UpdateOrnament_ExistingId_ReturnsOkWithUpdatedOrnament()
+        {
+            // Arrange
+            var ornamentId = 1;
+            var newOrnament = new OrnamentUpdateDto { Name = "UpdateName" };
+            var ornament = new OrnamentDto { Name = "TestName" };
+            _ornamentServiceMock.Setup(mock => mock.GetOrnamentById(ornamentId)).ReturnsAsync(ornament);
+            _ornamentServiceMock.Setup(mock => mock.UpdateOrnament(ornamentId, newOrnament)).ReturnsAsync(true);
+            _ornamentServiceMock.Setup(mock => mock.GetOrnamentById(ornamentId)).ReturnsAsync(ornament);
+
+            // Act
+            var result = await _controller.UpdateOrnament(ornamentId, newOrnament) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status200OK, result!.StatusCode);
+            Assert.Equal(ornament, result!.Value);
+        }
+
+        [Fact]
+        public async Task UpdateOrnament_NonExistingId_ReturnsNotFound()
+        {
+            // Arrange
+            var ornamentId = 1;
+            var newOrnament = new OrnamentUpdateDto { Name = "UpdateName" };
+            _ornamentServiceMock.Setup(mock => mock.GetOrnamentById(ornamentId)).ReturnsAsync((OrnamentDto)null!);
+
+            // Act
+            var result = await _controller.UpdateOrnament(ornamentId, newOrnament) as NotFoundObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status404NotFound, result!.StatusCode);
+            Assert.Equal($"Ornament with ID = {ornamentId} does not exist.", result!.Value);
+        }
+
+        [Fact]
+        public async Task UpdateOrnament_UpdateFails_ReturnsBadRequest()
+        {
+            // Arrange
+            var ornamentId = 1;
+            var newOrnament = new OrnamentUpdateDto { Name = "UpdateName" };
+            var ornament = new OrnamentDto { Name = "TestName" };
+            _ornamentServiceMock.Setup(mock => mock.GetOrnamentById(ornamentId)).ReturnsAsync(ornament);
+            _ornamentServiceMock.Setup(mock => mock.UpdateOrnament(ornamentId, newOrnament)).ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.UpdateOrnament(ornamentId, newOrnament) as BadRequestResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status400BadRequest, result!.StatusCode);
+        }
+
+        [Fact]
+        public async Task UpdateOrnament_ExceptionThrown_ReturnsInternalServerError()
+        {
+            // Arrange
+            var ornamentId = 1;
+            var newOrnament = new OrnamentUpdateDto { Name = "UpdateName" };
+            var ornament = new OrnamentDto { Name = "TestName" };
+            _ornamentServiceMock.Setup(mock => mock.GetOrnamentById(ornamentId)).ReturnsAsync(ornament);
+            _ornamentServiceMock.Setup(mock => mock.UpdateOrnament(ornamentId, newOrnament)).ThrowsAsync(new Exception("Some error message"));
+
+            // Act
+            var result = await _controller.UpdateOrnament(ornamentId, newOrnament) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result!.StatusCode);
+            Assert.Equal("An error occurred while updating the Ornament.", result!.Value);
+        }
+
+        [Fact]
+        public async Task DeleteOrnament_ExistingId_ReturnsOk()
+        {
+            // Arrange
+            var ornamentId = 1;
+            _ornamentServiceMock.Setup(mock => mock.DeleteOrnament(ornamentId)).ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.DeleteOrnament(ornamentId) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+            Assert.Equal($"Successfully deleted ornament with ID {ornamentId}.", result.Value);
+        }
+
+        [Fact]
+        public async Task DeleteOrnament_NonExistingId_ReturnsNotFound()
+        {
+            // Arrange
+            var ornamentId = 1;
+            _ornamentServiceMock.Setup(mock => mock.DeleteOrnament(ornamentId)).ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.DeleteOrnament(ornamentId) as BadRequestResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status400BadRequest, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task DeleteOrnament_InternalServerError_ReturnsStatusCode500()
+        {
+            // Arrange
+            var ornamentId = 1;
+            _ornamentServiceMock.Setup(mock => mock.DeleteOrnament(ornamentId)).ThrowsAsync(new Exception("Some error occurred."));
+
+            // Act
+            var result = await _controller.DeleteOrnament(ornamentId) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result.StatusCode);
+            Assert.Equal("An error occurred while deleting the Ornament.", result.Value);
+        }
+
+        [Fact]
+        public async Task DeleteOrnament_ExceptionThrown_ReturnsInternalServerError()
+        {
+            // Arrange
+            var ornamentId = 1;
+            _ornamentServiceMock.Setup(mock => mock.DeleteOrnament(ornamentId)).ThrowsAsync(new Exception("Some error message"));
+
+            // Act
+            var result = await _controller.DeleteOrnament(ornamentId) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result!.StatusCode);
+            Assert.Equal("An error occurred while deleting the Ornament.", result!.Value);
+        }
+    }
+}
---
+++ trailblazers-api/trailblazers-api-tests/Controllers/RelicControllerTests.cs
@@ -0,0 +1,324 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using trailblazers_api.Dtos.Relics;
+using trailblazers_api.Services.Relics;
+using Xunit;
+
+namespace trailblazers_api.Controllers.Tests
+{
+    public class RelicsControllerTests
+    {
+        private readonly Mock<ILogger<RelicsController>> _loggerMock;
+        private readonly Mock<IRelicService> _relicServiceMock;
+        private readonly RelicsController _controller;
+
+        public RelicsControllerTests()
+        {
+            _loggerMock = new Mock<ILogger<RelicsController>>();
+            _relicServiceMock = new Mock<IRelicService>();
+            _controller = new RelicsController(_loggerMock.Object, _relicServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task CreateRelic_ValidRelic_ReturnsCreatedAtRoute()
+        {
+            // Arrange
+            var relicCreationDto = new RelicCreationDto { Name = "TestName" };
+            var newRelic = new RelicDto { Name = "TestName" };
+            _relicServiceMock.Setup(mock => mock.CreateRelic(relicCreationDto)).ReturnsAsync(newRelic);
+
+            // Act
+            var result = await _controller.CreateRelic(relicCreationDto) as CreatedAtRouteResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status201Created, result!.StatusCode);
+            Assert.Equal("GetRelicById", result!.RouteName);
+            Assert.Equal(newRelic, result!.Value);
+        }
+
+        [Fact]
+        public async Task CreateRelic_InvalidRelic_ReturnsBadRequest()
+        {
+            // Arrange
+            var relicCreationDto = new RelicCreationDto { Name = "TestName" };
+            _relicServiceMock.Setup(mock => mock.CreateRelic(relicCreationDto)).ReturnsAsync((RelicDto)null!);
+
+            // Act
+            var result = await _controller.CreateRelic(relicCreationDto) as BadRequestObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status400BadRequest, result!.StatusCode);
+            Assert.Equal("Relic cannot be created.", result!.Value);
+        }
+
+        [Fact]
+        public async Task CreateRelic_ExceptionThrown_ReturnsInternalServerError()
+        {
+            // Arrange
+            var relicCreationDto = new RelicCreationDto { Name = "TestName" };
+            _relicServiceMock.Setup(mock => mock.CreateRelic(relicCreationDto)).ThrowsAsync(new Exception("Some error message"));
+
+            // Act
+            var result = await _controller.CreateRelic(relicCreationDto) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result!.StatusCode);
+            Assert.Equal("An error occurred while creating the Relic.", result!.Value);
+        }
+
+        [Fact]
+        public async Task GetAllRelics_NoName_ReturnsOkWithRelics()
+        {
+            // Arrange
+            string? name = null;
+            var relics = new List<RelicDto> { new RelicDto { Name = "Name" } };
+            _relicServiceMock.Setup(mock => mock.GetAllRelics()).ReturnsAsync(relics);
+
+            // Act
+            var result = await _controller.GetAllRelics(name) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status200OK, result!.StatusCode);
+            Assert.Equal(relics, result!.Value);
+        }
+
+        [Fact]
+        public async Task GetAllRelics_WithName_ReturnsOkWithRelic()
+        {
+            // Arrange
+            var name = "SomeRelicName";
+            var relic = new RelicDto { Name = "TestName" };
+            _relicServiceMock.Setup(mock => mock.GetRelicByName(name)).ReturnsAsync(relic);
+
+            // Act
+            var result = await _controller.GetAllRelics(name) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status200OK, result!.StatusCode);
+            Assert.Equal(relic, result!.Value);
+        }
+
+        [Fact]
+        public async Task GetAllRelics_NoRelics_ReturnsNoContent()
+        {
+            // Arrange
+            string? name = null;
+            _relicServiceMock.Setup(mock => mock.GetAllRelics()).ReturnsAsync(new List<RelicDto>());
+
+            // Act
+            var result = await _controller.GetAllRelics(name) as NoContentResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status204NoContent, result!.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAllRelics_ExceptionThrown_ReturnsInternalServerError()
+        {
+            // Arrange
+            string? name = null;
+            _relicServiceMock.Setup(mock => mock.GetAllRelics()).ThrowsAsync(new Exception("Some error message"));
+
+            // Act
+            var result = await _controller.GetAllRelics(name) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result!.StatusCode);
+            Assert.Equal("An error occurred while retrieving the Relics.", result!.Value);
+        }
+
+        [Fact]
+        public async Task GetRelicById_ExistingId_ReturnsOkWithRelic()
+        {
+            // Arrange
+            var relicId = 1;
+            var relic = new RelicDto { Name = "TestName" };
+            _relicServiceMock.Setup(mock => mock.GetRelicById(relicId)).ReturnsAsync(relic);
+
+            // Act
+            var result = await _controller.GetRelicById(relicId) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status200OK, result!.StatusCode);
+            Assert.Equal(relic, result!.Value);
+        }
+
+        [Fact]
+        public async Task GetRelicById_NonExistingId_ReturnsNoContent()
+        {
+            // Arrange
+            var relicId = 1;
+            _relicServiceMock.Setup(mock => mock.GetRelicById(relicId)).ReturnsAsync((RelicDto)null!);
+
+            // Act
+            var result = await _controller.GetRelicById(relicId) as NoContentResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status204NoContent, result!.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetRelicById_ExceptionThrown_ReturnsInternalServerError()
+        {
+            // Arrange
+            var relicId = 1;
+            _relicServiceMock.Setup(mock => mock.GetRelicById(relicId)).ThrowsAsync(new Exception("Some error message"));
+
+            // Act
+            var result = await _controller.GetRelicById(relicId) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result!.StatusCode);
+            Assert.Equal("An error occurred while retrieving the Relic.", result!.Value);
+        }
+
+        [Fact]
+        public async Task UpdateRelic_ExistingId_ReturnsOkWithUpdatedRelic()
+        {
+            // Arrange
+            var relicId = 1;
+            var newRelic = new RelicUpdateDto { Name = "UpdateName" };
+            var relic = new RelicDto { Name = "TestName" };
+            _relicServiceMock.Setup(mock => mock.GetRelicById(relicId)).ReturnsAsync(relic);
+            _relicServiceMock.Setup(mock => mock.UpdateRelic(relicId, newRelic)).ReturnsAsync(true);
+            _relicServiceMock.Setup(mock => mock.GetRelicById(relicId)).ReturnsAsync(relic);
+
+            // Act
+            var result = await _controller.UpdateRelic(relicId, newRelic) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status200OK, result!.StatusCode);
+            Assert.Equal(relic, result!.Value);
+        }
+
+        [Fact]
+        public async Task UpdateRelic_NonExistingId_ReturnsNotFound()
+        {
+            // Arrange
+            var relicId = 1;
+            var newRelic = new RelicUpdateDto { Name = "UpdateName" };
+            _relicServiceMock.Setup(mock => mock.GetRelicById(relicId)).ReturnsAsync((RelicDto)null!);
+
+            // Act
+            var result = await _controller.UpdateRelic(relicId, newRelic) as NotFoundObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status404NotFound, result!.StatusCode);
+            Assert.Equal($"Relic with ID = {relicId} does not exist.", result!.Value);
+        }
+
+        [Fact]
+        public async Task UpdateRelic_UpdateFails_ReturnsBadRequest()
+        {
+            // Arrange
+            var relicId = 1;
+            var newRelic = new RelicUpdateDto { Name = "UpdateName" };
+            var relic = new RelicDto { Name = "TestName" };
+            _relicServiceMock.Setup(mock => mock.GetRelicById(relicId)).ReturnsAsync(relic);
+            _relicServiceMock.Setup(mock => mock.UpdateRelic(relicId, newRelic)).ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.UpdateRelic(relicId, newRelic) as BadRequestResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status400BadRequest, result!.StatusCode);
+        }
+
+        [Fact]
+        public async Task UpdateRelic_ExceptionThrown_ReturnsInternalServerError()
+        {
+            // Arrange
+            var relicId = 1;
+            var newRelic = new RelicUpdateDto { Name = "UpdateName" };
+            var relic = new RelicDto { Name = "TestName" };
+            _relicServiceMock.Setup(mock => mock.GetRelicById(relicId)).ReturnsAsync(relic);
+            _relicServiceMock.Setup(mock => mock.UpdateRelic(relicId, newRelic)).ThrowsAsync(new Exception("Some error message"));
+
+            // Act
+            var result = await _controller.UpdateRelic(relicId, newRelic) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result!.StatusCode);
+            Assert.Equal("An error occurred while updating the Relic.", result!.Value);
+        }
+
+        [Fact]
+        public async Task DeleteRelic_ExistingId_ReturnsOk()
+        {
+            // Arrange
+            var relicId = 1;
+            _relicServiceMock.Setup(mock => mock.DeleteRelic(relicId)).ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.DeleteRelic(relicId) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status200OK, result!.StatusCode);
+            Assert.Equal($"Successfully deleted relic with ID {relicId}.", result!.Value);
+        }
+
+        [Fact]
+        public async Task DeleteRelic_NonExistingId_ReturnsNotFound()
+        {
+            // Arrange
+            var relicId = 1;
+            _relicServiceMock.Setup(mock => mock.DeleteRelic(relicId)).ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.DeleteRelic(relicId) as BadRequestResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status400BadRequest, result!.StatusCode);
+        }
+
+        [Fact]
+        public async Task DeleteRelic_InternalServerError_ReturnsStatusCode500()
+        {
+            // Arrange
+            var relicId = 1;
+            _relicServiceMock.Setup(mock => mock.DeleteRelic(relicId)).ThrowsAsync(new Exception("Some error occurred."));
+
+            // Act
+            var result = await _controller.DeleteRelic(relicId) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result!.StatusCode);
+            Assert.Equal("An error occurred while deleting the Relic.", result!.Value);
+        }
+
+        [Fact]
+        public async Task DeleteRelic_ExceptionThrown_ReturnsInternalServerError()
+        {
+            // Arrange
+            var relicId = 1;
+            _relicServiceMock.Setup(mock => mock.DeleteRelic(relicId)).ThrowsAsync(new Exception("Some error message"));
+
+            // Act
+            var result = await _controller.DeleteRelic(relicId) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result!.StatusCode);
+            Assert.Equal("An error occurred while deleting the Relic.", result!.Value);
+        }
+    }
+}
