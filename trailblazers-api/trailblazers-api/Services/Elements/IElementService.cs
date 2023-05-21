1b7f88cf3369fa345df2db1ebc9c49cd14b0252e
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-22 00:25:42 +0800

    Update doc

--- trailblazers-api/trailblazers-api/Services/Elements/IElementService.cs
+++ trailblazers-api/trailblazers-api/Services/Elements/IElementService.cs
@@ -12,31 +12,37 @@ namespace trailblazers_api.Services.Elements
         Task<ElementDto?> CreateElement(ElementCreationDto element);
 
         /// <summary>
-        /// Gets All the Element in the Database.
+        /// Gets all the Elements in the Database.
         /// </summary>
-        /// <returns>All Elements in the database></returns>
+        /// <returns>All Elements in the database.</returns>
         Task<IEnumerable<ElementDto>> GetAllElements();
 
         /// <summary>
-        /// Gets the first Element with the given id
+        /// Gets the Element with the given ID.
         /// </summary>
-        /// <param name="id">Id of Element to be retrieved</param>
-        /// <returns>Element with given id</returns>
+        /// <param name="id">ID of the Element to be retrieved.</param>
+        /// <returns>The Element with the given ID, or null if not found.</returns>
         Task<ElementDto?> GetElementById(int id);
 
+        /// <summary>
+        /// Gets the Element with the given name.
+        /// </summary>
+        /// <param name="name">Name of the Element to be retrieved.</param>
+        /// <returns>The Element with the given name, or null if not found.</returns>
         Task<ElementDto?> GetElementByName(string name);
 
         /// <summary>
-        /// Updates an existing element in the database.
+        /// Updates an existing Element in the database.
         /// </summary>
-        /// <param name="element">The updated element.</param>
+        /// <param name="id">ID of the Element to be updated.</param>
+        /// <param name="element">The updated Element object.</param>
         /// <returns>True if the update was successful, false otherwise.</returns>
         Task<bool> UpdateElement(int id, ElementUpdateDto element);
 
         /// <summary>
-        /// Deletes an element from the database.
+        /// Deletes an Element from the database.
         /// </summary>
-        /// <param name="id">The ID of the element to delete.</param>
+        /// <param name="id">The ID of the Element to delete.</param>
         /// <returns>True if the deletion was successful, false otherwise.</returns>
         Task<bool> DeleteElement(int id);
     }
