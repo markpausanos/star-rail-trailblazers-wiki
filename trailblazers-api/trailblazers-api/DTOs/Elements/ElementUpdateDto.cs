ca47f663ccc53017cb19d9eccb78be5c08317022
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-22 00:24:46 +0800

    Add Elementdtos

--- trailblazers-api/trailblazers-api/DTOs/Elements/ElementUpdateDto.cs
+++ trailblazers-api/trailblazers-api/DTOs/Elements/ElementUpdateDto.cs
@@ -1,28 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace trailblazers_api.Dtos.Elements
+﻿namespace trailblazers_api.Dtos.Elements
 {
-    /// <summary>
-    /// Element update DTO class
-    /// </summary>
     public class ElementUpdateDto
     {
-        /// <summary>
-        /// Id of the Element
-        /// </summary>
-        [Required(ErrorMessage = "Id is a required field")]
-        public int Id { get; set; }
-
-        /// <summary>
-        /// Name of the Element
-        /// </summary>
-        [Required(ErrorMessage = "Name is a required field")]
-        public string Name { get; set; }
-
-        /// <summary>
-        /// Directory path of the Element image
-        /// </summary>
-        [Required(ErrorMessage = "Image is a required field")]
-        public string Image { get; set; }
+        public string? Name { get; set; }
+        public string? Image { get; set; }
     }
 }
