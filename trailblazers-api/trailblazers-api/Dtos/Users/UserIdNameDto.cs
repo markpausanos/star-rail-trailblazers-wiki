3f6663fa4b92ac35a27070abe709765d5c525888
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-22 00:24:59 +0800

    Add User Dtos

--- trailblazers-api/trailblazers-api/Dtos/Users/UserIdNameDto.cs
+++ trailblazers-api/trailblazers-api/Dtos/Users/UserIdNameDto.cs
@@ -1,9 +1,8 @@
 ﻿namespace trailblazers_api.Dtos.Users
 {
-    public class UserAccessDto
+    public class UserIdNameDto
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public string? Role { get; set; }
     }
 }
