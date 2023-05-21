13a53bbce09d47f692d09f250e7f351701de6c73
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-22 00:37:42 +0800

    Update Trace repo

--- trailblazers-api/trailblazers-api/Repositories/Traces/TraceRepository.cs
+++ trailblazers-api/trailblazers-api/Repositories/Traces/TraceRepository.cs
@@ -12,22 +12,23 @@ namespace trailblazers_api.Repositories.Traces
         {
             _context = context;
         }
-        public async Task<int> CreateTrace(Trace ascension)
+        public async Task<int> CreateTrace(Trace trace)
         {
-            var sql = "INSERT INTO Trace (Name, Description) VALUES (@Name, @Description); " +
+            var sql = "INSERT INTO Traces (Name, Description, Image) VALUES (@Name, @Description, @Image); " +
                       "SELECT SCOPE_IDENTITY();";
 
             using (var con = _context.CreateConnection())
             {
-                return await con.ExecuteScalarAsync<int>(sql, new { ascension.Name, ascension.Description });
+                return await con.ExecuteScalarAsync<int>(sql, new { trace.Name, trace.Description, trace.Image });
             }
         }
-        public async Task<IEnumerable<Trace>> GetAllTrace()
+
+        public async Task<IEnumerable<Trace>> GetAllTraces()
         {
             var sql = @"SELECT tr.*, t.*
                 FROM Trace tr
                 LEFT JOIN Trailblazer t ON tr.TrailblazerId = t.Id
-                WHERE tr.IsDeleted = 0;";
+                WHERE e.IsDeleted = 0;";
 
             using (var con = _context.CreateConnection())
             {
@@ -49,16 +50,18 @@ namespace trailblazers_api.Repositories.Traces
                 }, splitOn: "Id");
             }
         }
-        public async Task<IEnumerable<Trace>> GetTraceByTrailblazerId(int trailblazerId)
+
+        public async Task<IEnumerable<Trace>> GetTracesByTrailblazerId(int trailblazerId)
         {
-            var sql = @"SELECT tr.*, t.*
-                FROM Trace tr
-                LEFT JOIN Trailblazer t ON tr.TrailblazerId = t.Id
-                WHERE tr.IsDeleted = 0 AND tr.TrailblazerId = @TrailblazerId;";
+            var sql = "SELECT tr.*, t.* " +
+                      "FROM Trace tr " +
+                      "LEFT JOIN Trailblazer t ON tr.TrailblazerId = t.Id " +
+                      "WHERE tr.TrailblazerId = @TrailblazerId AND e.IsDeleted = 0;";
 
             using (var con = _context.CreateConnection())
             {
                 var traceDict = new Dictionary<int, Trace>();
+
                 return await con.QueryAsync<Trace, Trailblazer, Trace>(sql, (tr, t) =>
                 {
                     if (!traceDict.TryGetValue(tr.Id, out var trace))
@@ -77,19 +80,61 @@ namespace trailblazers_api.Repositories.Traces
             }
         }
 
-        public async Task<bool> UpdateTrace(Trace ascension)
+        public async Task<Trace?> GetTraceById(int id)
         {
-            var sql = "UPDATE Trace SET Description = @Description WHERE Id = @Id;";
+            var sql = @"SELECT tr.*, t.*
+                FROM Trace tr
+                LEFT JOIN Trailblazer t ON tr.TrailblazerId = t.Id
+                WHERE e.IsDeleted = 0 AND e.Id = @Id;";
+
+            using (var con = _context.CreateConnection())
+            {
+                var traceDict = new Dictionary<int, Trace>();
+                var traces = await con.QueryAsync<Trace, Trailblazer, Trace>(
+                    sql,
+                    (tr, t) =>
+                    {
+                        if (!traceDict.TryGetValue(tr.Id, out var trace))
+                        {
+                            trace = tr;
+                            trace.Trailblazer = t;
+                            traceDict.Add(tr.Id, trace);
+                        }
+                        else if (trace.Trailblazer == null)
+                        {
+                            trace.Trailblazer = t;
+                        }
+                        return trace;
+                    },
+                new { id },
+                splitOn: "Id");
+
+                return traces.FirstOrDefault();
+            }
+        }
+
+        public async Task<bool> UpdateTrace(Trace trace)
+        {
+            var sql = "UPDATE Trace SET Name = @Name, " +
+                "Description = @Description, Image = @Image, Order = @Order WHERE Id = @Id;";
 
 
             using (var con = _context.CreateConnection())
             {
-                return await con.ExecuteAsync(sql, new { ascension.Description, ascension.Id }) > 0;
+                return await con.ExecuteAsync(sql, new
+                {
+                    trace.Name,
+                    trace.Description,
+                    trace.Image,
+                    trace.Order,
+                    trace.Id
+                }) > 0;
             }
         }
         public async Task<bool> DeleteTrace(int id)
         {
             var sql = "UPDATE Trace SET IsDeleted = 1 WHERE Id = @Id;";
+
             using (var con = _context.CreateConnection())
             {
                 return await con.ExecuteAsync(sql, new { id }) > 0;
