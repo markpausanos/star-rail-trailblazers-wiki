fa389f1564bd91fc35a732b5e36ab5ecedb347fb
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-23 03:29:51 +0800

    Add TrailblazersService

---
+++ trailblazers-react/src/services/TrailblazersService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import config from './config';
+
+const BASE_URL = `${config.API_URL}/Trailblazers`;
+
+const TrailblazersService = {
+    create: (trailblazer) => axios.post(BASE_URL, trailblazer),
+    list: () => axios.get(BASE_URL),
+    retrieveById: (id) => axios.get(`${BASE_URL}/${id}`),
+    update: (id, updatedTrailblazer) => axios.put(`${BASE_URL}/${id}`, updatedTrailblazer)
+}
+
+export default TrailblazersService;
