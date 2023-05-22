06b0f04bc9e456a6ee060a5fd004d37a06002d77
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-23 03:30:22 +0800

    Add index

---
+++ trailblazers-react/src/services/index.js
@@ -0,0 +1,6 @@
+export { default as BuildsService } from './BuildsService';
+export { default as LightconesService } from './LightconesService';
+export { default as OrnamentsService } from './OrnamentsService';
+export { default as RelicsService } from './RelicsService';
+export { default as TrailblazersService } from './TrailblazersService';
+export { default as UsersService } from './UsersService';
