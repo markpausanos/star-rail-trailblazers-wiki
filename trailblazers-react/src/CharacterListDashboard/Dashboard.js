94f662777ebd117590bce6c2aa3e4cedab71bf6a
Shane Denney Cuizon <shanedenney.cuizon@cit.edu>
2023-05-23 11:30:09 +0800

    Add nav bar in dashboard

--- trailblazers-react/src/CharacterListDashboard/Dashboard.js
+++ trailblazers-react/src/CharacterListDashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { useNavigate } from "react-router";
 import './Dashboard.css';
 import CategoryTab from './CategoryTab';
 import CharacterPageList from './CharacterPageList';
 import LightConePage from './LightConePage';
 import Relics from './Relics';
 import Ornaments from './Ornaments';
+import NavBar from '../NavBar';
 
 /**
  * 
@@ -12,7 +14,11 @@ import Ornaments from './Ornaments';
  */
 function Dashboard() {
   const [activeItem, setActiveItem] = useState('characters');
-
+  const[isOpen,setIsOpen]=useState(false);
+    const navigate = useNavigate();
+  const toggle=()=>{
+    setIsOpen(!isOpen);
+  }
   const handleClick = (event) => {
     setActiveItem(event.target.textContent.toLowerCase());
   };
@@ -20,6 +26,12 @@ function Dashboard() {
   return (
     <div className='dashboard'>
         <CategoryTab OnClickHandle={handleClick} activeItem={activeItem}/>
+        <div style={{width: isOpen ? "15%" : "0%"}} className="navbar">
+              {isOpen && <div className='option' onClick={() => navigate('/dashboard')}>Dashboard</div>}
+              {isOpen && <div className='option' onClick={() => navigate('/character-showcase')}>Character Showcase</div>}
+              {isOpen && <div className='option' onClick={() => navigate('/')}>SignOut</div>}
+          </div>
+        <div className='menu'onClick={toggle}>Menu</div>
         <div className='contentDashboard'>
           {activeItem === 'characters' && <CharacterPageList />}
           {activeItem === 'light cones' && <LightConePage />}
