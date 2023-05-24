f488133de4c9d9f73e6ce82faa2a52aa7e1f4a90
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-25 02:05:33 +0800

    Add new modules

--- trailblazers-react/src/App.js
+++ trailblazers-react/src/App.js
@@ -1,19 +1,38 @@
-import './App.css';
-import { Routes, Route } from 'react-router';
-import Login from './Login-Signup/Login';
-import Dashboard from './CharacterListDashboard/Dashboard';
-import SignUp from './Login-Signup/SignUp';
-import AdminDashboard from './AdminScreen/AdminDashboard';
-import TeamShowcase from './TeamShowcaseFeature/TeamShowcase';
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Cookies from "universal-cookie";
+import {
+  Builds,
+  Lightcones,
+  Login,
+  Ornaments,
+  Relics,
+  SignUp,
+} from "./screens";
+import "./styles/App.scss";
+import Dashboard from "./screens/user/Dashboard";
+
+const cookies = new Cookies();
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Login />}></Route>
-      <Route path="sign-up" element={<SignUp />}></Route>
-      <Route path="dashboard" element={<Dashboard />}></Route>
-      <Route path="admindashboard" element={<AdminDashboard />}></Route>
-      <Route path ="teamshowcase" element={<TeamShowcase/>}/>
+      <Route path="/" exact element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/characters" element={<Dashboard />} />
+      <Route path="/lightcones" element={<Lightcones />} />
+      <Route path="/ornaments" element={<Ornaments />} />
+      <Route path="/relics" element={<Relics />} />
+      <Route path="/builds" element={<Builds />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
