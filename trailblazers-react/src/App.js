c0344c72a5a955b245e3d137bc0017f92e3f07b7
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2023-05-25 02:15:58 +0800

    Fix

--- trailblazers-react/src/App.js
+++ trailblazers-react/src/App.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-/* eslint-disable no-unused-vars */
-import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Cookies from "universal-cookie";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import {
   Builds,
   Lightcones,
@@ -19,23 +11,9 @@ import {
 import "./styles/App.scss";
 import Dashboard from "./screens/user/Dashboard";
 
-const cookies = new Cookies();
-=======
-import './App.css';
-import { Routes, Route } from 'react-router';
-import Login from './Login-Signup/Login';
-import Dashboard from './CharacterListDashboard/Dashboard';
-import SignUp from './Login-Signup/SignUp';
-import CharacterShowcasePage from './CharacterShowcaseFeature/CharacterShowcasePage';
-
-import AdminDashboard from './AdminScreen/AdminDashboard';
-
->>>>>>> a44b32fff4f67009ce2cfd8923b5a2bc716b8585
-
 function App() {
   return (
     <Routes>
-<<<<<<< HEAD
       <Route path="/" exact element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/sign-up" element={<SignUp />} />
@@ -46,13 +24,6 @@ function App() {
       <Route path="/relics" element={<Relics />} />
       <Route path="/builds" element={<Builds />} />
       <Route path="*" element={<Navigate to="/" />} />
-=======
-      <Route path='/' element={<Login />}></Route>
-      <Route path="sign-up" element={<SignUp />}></Route>
-      <Route path="dashboard" element={<Dashboard />}></Route>
-      <Route path ="character-showcase" element={<CharacterShowcasePage/>}/>
-      <Route path="admindashboard" element={<AdminDashboard />}></Route>
->>>>>>> a44b32fff4f67009ce2cfd8923b5a2bc716b8585
     </Routes>
   );
 }
